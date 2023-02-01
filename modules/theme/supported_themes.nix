54305d50fa88098ec1aef83ba2c3827780ccace5
NotAShelf <itsashelf@gmail.com>
2023-02-02 00:00:48 +0300

    feat: further plugin integration for catppuccin theme

--- modules/theme/supported_themes.nix
+++ modules/theme/supported_themes.nix
@@ -1,33 +1,47 @@
 {
   onedark = {
-    setup = { style ? "dark" }: ''
+    setup = {style ? "dark"}: ''
       -- OneDark theme
       require('onedark').setup {
         style = "${style}"
       }
       require('onedark').load()
     '';
-    styles = [ "dark" "darker" "cool" "deep" "warm" "warmer" ];
+    styles = ["dark" "darker" "cool" "deep" "warm" "warmer"];
   };
 
   tokyonight = {
-    setup = { style ? "night" }: ''
+    setup = {style ? "night"}: ''
       -- need to set style before colorscheme to apply
       vim.g.tokyonight_style = '${style}'
       vim.cmd[[colorscheme tokyonight]]
     '';
-    styles = [ "day" "night" "storm" ];
+    styles = ["day" "night" "storm"];
   };
 
   catppuccin = {
-    setup = { style ? "mocha" }: ''
+    setup = {style ? "mocha"}: ''
       -- Catppuccin theme
       require('catppuccin').setup {
-        flavour = "${style}"
+        flavour = "${style}",
+        transparent_background = true,
+        integrations = {
+      	  nvimtree = {
+      		  enabled = true,
+      		  transparent_panel = false,
+      		  show_root = true,
+      	  },
+
+          hop = true,
+      	  gitsigns = true,
+      	  telescope = true,
+      	  treesitter = true,
+      	  ts_rainbow = true,
+        },
       }
       -- setup must be called before loading
       vim.cmd.colorscheme "catppuccin"
     '';
-    styles = [ "latte" "frappe" "macchiato" "mocha" ];
+    styles = ["latte" "frappe" "macchiato" "mocha"];
   };
 }
