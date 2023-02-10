37d05187dfc9c0cc98f3af57e47dd9d35e0df366
Mihai Fufezan <fufexan@protonmail.com>
2023-02-10 19:50:20 +0200

    packages.nix: niceify

--- flake/packages.nix
+++ flake/packages.nix
@@ -1,41 +1,11 @@
 {inputs, ...}: {
-  # imports = [
-  #   inputs.flake-parts.flakeModules.easyOverlay
-  # ];
-
   perSystem = {
     system,
-    pkgs,
     config,
+    pkgs,
+    lib,
     ...
-  }: let
-    # inherit (import ../extra.nix inputs) neovimConfiguration mainConfig;
-
-    # tidalConfig = {
-    #   config.vim.tidal.enable = true;
-    # };
-
-    # buildPkg = pkgs: modules:
-    #   (neovimConfiguration {
-    #     inherit pkgs modules;
-    #   })
-    #   .neovim;
-
-    # nixConfig = mainConfig false;
-    # maximalConfig = mainConfig true;
-  in {
-    # overlayAttrs =
-    #   {
-    #     inherit neovimConfiguration;
-    #     neovim-nix = config.packages.nix;
-    #     neovim-maximal = config.packages.maximal;
-    #   }
-    #   // (
-    #     if !(builtins.elem system ["aarch64-darwin" "x86_64-darwin"])
-    #     then {neovim-tidal = config.packages.tidal;}
-    #     else {}
-    #   );
-
+  }: {
     packages = let
       docs = import ../docs {
         inherit pkgs;
@@ -50,14 +20,11 @@
         docs-json = docs.options.json;
 
         # nvim configs
-        # nix = buildPkg pkgs [nixConfig];
-        # maximal = buildPkg pkgs [maximalConfig];
         nix = config.legacyPackages.neovim-nix;
         maximal = config.legacyPackages.neovim-maximal;
       }
       // (
         if !(builtins.elem system ["aarch64-darwin" "x86_64-darwin"])
-        # then {tidal = buildPkg pkgs [tidalConfig];}
         then {tidal = config.legacyPackages.neovim-tidal;}
         else {}
       );
