1ca507569d5f9034ab970e83173bb3eb177beb81
NotAShelf <itsashelf@gmail.com>
2023-02-28 10:54:35 +0300

    feat: update lsp config

--- modules/lsp/config.nix
+++ modules/lsp/config.nix
@@ -8,61 +8,6 @@ with lib;
 with builtins; let
   cfg = config.vim.lsp;
 in {
-  options.vim.lsp = {
-    enable = mkEnableOption "neovim lsp support";
-    formatOnSave = mkEnableOption "Format on save";
-    nix = {
-      enable = mkEnableOption "Nix LSP";
-      server = mkOption {
-        type = with types; enum ["rnix" "nil"];
-        default = "nil";
-        description = "Which LSP to use";
-      };
-
-      pkg = mkOption {
-        type = types.package;
-        default =
-          if (cfg.nix.server == "rnix")
-          then pkgs.rnix-lsp
-          else pkgs.nil;
-        description = "The LSP package to use";
-      };
-
-      formatter = mkOption {
-        type = with types; enum ["nixpkgs-fmt" "alejandra"];
-        default = "alejandra";
-        description = "Which nix formatter to use";
-      };
-    };
-    rust = {
-      enable = mkEnableOption "Rust LSP";
-      rustAnalyzerOpts = mkOption {
-        type = types.str;
-        default = ''
-          ["rust-analyzer"] = {
-            experimental = {
-              procAttrMacros = true,
-            },
-          },
-        '';
-        description = "options to pass to rust analyzer";
-      };
-    };
-    python = mkEnableOption "Python LSP";
-    clang = {
-      enable = mkEnableOption "C language LSP";
-      c_header = mkEnableOption "C syntax header files";
-      cclsOpts = mkOption {
-        type = types.str;
-        default = "";
-      };
-    };
-    sql = mkEnableOption "SQL Language LSP";
-    go = mkEnableOption "Go language LSP";
-    ts = mkEnableOption "TS language LSP";
-    zig.enable = mkEnableOption "Zig language LSP";
-  };
-
   config = mkIf cfg.enable (
     let
       writeIf = cond: msg:
