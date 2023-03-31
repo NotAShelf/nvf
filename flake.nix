39772707fc22c6c7291cca8e87c7d5ca07f0a071
NotAShelf <itsashelf@gmail.com>
2023-04-01 01:44:47 +0300

    Revert "dev: try legacypackages"
    This reverts commit 317ff16bb7d71f49ecbba0550a085463deb301a5.

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
 
         homeManagerModules.default = {
           imports = [
-            (import ./lib/module self.config)
+            (import ./lib/module self.packages)
           ];
         };
       };
