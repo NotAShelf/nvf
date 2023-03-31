ef6341dd33c28e9517e05f17385d7ab88ca7af01
NotAShelf <itsashelf@gmail.com>
2023-04-01 01:01:22 +0300

    dev: its pkgs.system

--- lib/module/default.nix
+++ lib/module/default.nix
@@ -8,7 +8,7 @@ packages: {
 }:
 with lib; let
   cfg = config.programs.neovim-flake;
-  set = self.packages.maximal {mainConfig = cfg.settings;};
+  set = self.packages.${pkgs.system}.maximal {mainConfig = cfg.settings;};
 in {
   meta.maintainers = [maintainers.notashelf];
 
