46c602f74dca9419f6070e323390e5d392eac0d7
n3oney <neo@neoney.dev>
2023-05-02 23:22:35 +0200

    feat: add nvim-code-action-menu keybindings

--- modules/lsp/nvim-code-action-menu/config.nix
+++ modules/lsp/nvim-code-action-menu/config.nix
@@ -6,12 +6,15 @@
 with lib;
 with builtins; let
   cfg = config.vim.lsp;
+
+  self = import ./nvim-code-action-menu.nix {inherit lib;};
+
+  mappingDefinitions = self.options.vim.lsp.nvimCodeActionMenu.mappings;
+  mappings = addDescriptionsToMappings cfg.nvimCodeActionMenu.mappings mappingDefinitions;
 in {
   config = mkIf (cfg.enable && cfg.nvimCodeActionMenu.enable) {
     vim.startPlugins = ["nvim-code-action-menu"];
 
-    vim.maps.normal = {
-      "<silent><leader>ca" = {action = ":CodeActionMenu<CR>";};
-    };
+    vim.maps.normal = mkSetBinding mappings.open ":CodeActionMenu<CR>";
   };
 }
--- modules/lsp/nvim-code-action-menu/nvim-code-action-menu.nix
+++ modules/lsp/nvim-code-action-menu/nvim-code-action-menu.nix
@@ -1,13 +1,12 @@
-{
-  config,
-  lib,
-  ...
-}:
-with lib;
-with builtins; {
+{lib, ...}:
+with lib; {
   options.vim.lsp = {
     nvimCodeActionMenu = {
       enable = mkEnableOption "Enable nvim code action menu";
+
+      mappings = {
+        open = mkMappingOption "Open code action menu [nvim-code-action-menu]" "<leader>ca";
+      };
     };
   };
 }
