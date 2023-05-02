dce3d762d9c060306bcd820e8a5cf30de190f127
n3oney <neo@neoney.dev>
2023-05-02 22:12:45 +0200

    feat: add gesture.nvim keybindings

--- modules/utility/gestures/gesture-nvim/gesture-nvim.nix
+++ modules/utility/gestures/gesture-nvim/gesture-nvim.nix
@@ -1,11 +1,11 @@
-{
-  config,
-  lib,
-  ...
-}:
-with lib;
-with builtins; {
+{lib, ...}:
+with lib; {
   options.vim.gestures.gesture-nvim = {
     enable = mkEnableOption "Enable gesture-nvim plugin";
+
+    mappings = {
+      draw = mkMappingOption "Start drawing [gesture.nvim]" "<LeftDrag>";
+      finish = mkMappingOption "Finish drawing [gesture.nvim]" "<LeftRelease>";
+    };
   };
 }
