20c184178ea198e4e1aec4fe36c763a94eb98e11
NotAShelf <itsashelf@gmail.com>
2023-05-10 10:44:25 +0300

    fix: update deprecated buffer_close_icon

--- modules/tabline/nvim-bufferline/config.nix
+++ modules/tabline/nvim-bufferline/config.nix
@@ -51,7 +51,7 @@ in {
                 style = 'icon',
                 indicator_icon = '▎',
               },
-              buffer_close_icon = '',
+              buffer_close_icon = '󰅖',
               modified_icon = '●',
               close_icon = '',
               left_trunc_marker = '',
