79edfbdb475b74495957d6248ff36c24d42968ad
NotAShelf <itsashelf@gmail.com>
2023-06-06 03:25:43 +0300

    fix(illuminate): ignore NvimTree for highlighting

--- modules/ui/illuminate/config.nix
+++ modules/ui/illuminate/config.nix
@@ -10,8 +10,15 @@ in {
   config = mkIf cfg.enable {
     vim.startPlugins = ["vim-illuminate"];
 
-    vim.luaConfigRC.vim-illuminate =
-      nvim.dag.entryAnywhere ''
-      '';
+    vim.luaConfigRC.vim-illuminate = nvim.dag.entryAnywhere ''
+        require('illuminate').configure({
+          filetypes_denylist = {
+          'dirvish',
+          'fugitive',
+          'NvimTree',
+          'TelescopePrompt',
+        },
+      })
+    '';
   };
 }
