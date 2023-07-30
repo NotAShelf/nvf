7b89a3ad259a80cac49a4e7df7e5618ea2e99df7
NotAShelf <raf@notashelf.dev>
2023-07-30 19:13:05 +0300

    feat: provide configuration options for nvim-code-action-menu

--- modules/lsp/nvim-code-action-menu/config.nix
+++ modules/lsp/nvim-code-action-menu/config.nix
@@ -16,5 +16,15 @@ in {
     vim.startPlugins = ["nvim-code-action-menu"];
 
     vim.maps.normal = mkSetBinding mappings.open ":CodeActionMenu<CR>";
+
+    vim.luaConfigRC.code-action-menu = nvim.dag.entryAnywhere ''
+      -- border configuration
+      vim.g.code_action_menu_window_border = '${config.vim.ui.borders.plugins.code-action-menu.style}'
+
+      -- show individual sections of the code action menu
+      ${lib.optionalString (cfg.nvimCodeActionMenu.show.details) "vim.g.code_action_menu_show_details = true"}
+      ${lib.optionalString (cfg.nvimCodeActionMenu.show.diff) "vim.g.code_action_menu_show_diff = true"}
+      ${lib.optionalString (cfg.nvimCodeActionMenu.show.actionKind) "vim.g.code_action_menu_show_action_kind = true"}
+    '';
   };
 }
