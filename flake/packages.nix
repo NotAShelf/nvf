52ea8a9f459735c057cf286ccede8eecd291b10f
NotAShelf <raf@notashelf.dev>
2023-10-20 16:28:35 +0300

    flake: provide containerized builds

--- flake/packages.nix
+++ flake/packages.nix
@@ -1,6 +1,5 @@
 {inputs, ...}: {
   perSystem = {
-    self',
     system,
     config,
     pkgs,
@@ -19,16 +18,43 @@
         docs-manpages = docs.manPages;
         docs-json = docs.options.json;
 
+        # Build and open the built manual in your system browser
         docs-html-wrapped = pkgs.writeScriptBin "docs-html-wrapped" ''
           #!${pkgs.stdenv.shell}
           # use xdg-open to open the docs in the browser
           ${pkgs.xdg_utils}/bin/xdg-open ${docs.manual.html}
         '';
 
-        # nvim configs
+        # Exposed neovim configurations
         nix = config.legacyPackages.neovim-nix;
         maximal = config.legacyPackages.neovim-maximal;
         default = config.legacyPackages.neovim-nix;
+
+        # Publisged docker images
+        docker-nix = let
+          inherit (pkgs) bash gitFull buildEnv dockerTools;
+          inherit (config.legacyPackages) neovim-nix;
+        in
+          dockerTools.buildImage {
+            name = "neovim-flake-nix";
+            tag = "latest";
+
+            copyToRoot = buildEnv {
+              name = "neovim-root";
+              pathsToLink = ["/bin"];
+              paths = [
+                neovim-nix
+                gitFull
+                bash
+              ];
+            };
+
+            config = {
+              Cmd = ["${neovim-nix}/bin/nvim"];
+              WorkingDir = "/home/neovim/demo";
+              Volumes = {"/home/neovim/demo" = {};};
+            };
+          };
       }
       // (
         if !(builtins.elem system ["aarch64-darwin" "x86_64-darwin"])
