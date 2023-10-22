0e85c54a9ab33abf86bda676dc70456e3dea4a4f
NotAShelf <raf@notashelf.dev>
2023-10-22 18:10:28 +0300

    lib/languages: add back HM notice

--- lib/languages.nix
+++ lib/languages.nix
@@ -1,4 +1,7 @@
+# From home-manager: https://github.com/nix-community/home-manager/blob/master/modules/lib/booleans.nix
 {lib}: {
+  # Converts a boolean to a yes/no string. This is used in lots of
+  # configuration formats.
   diagnosticsToLua = {
     lang,
     config,
