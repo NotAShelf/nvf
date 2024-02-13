1bc9ec96c1ee1c9aa28373374774e4db5eb81898
NotAShelf <raf@notashelf.dev>
2024-02-13 22:32:27 +0300

    lib: drop deprecated booleans lib
    somehow slipped checks because lazy eval

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,5 @@
 {lib}: {
   dag = import ./dag.nix {inherit lib;};
-  booleans = import ./booleans.nix {inherit lib;};
   types = import ./types {inherit lib;};
   languages = import ./languages.nix {inherit lib;};
   lua = import ./lua.nix {inherit lib;};
