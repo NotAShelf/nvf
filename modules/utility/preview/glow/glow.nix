054ade00759e951ec3956b868a57d7fd1d34ca00
NotAShelf <raf@notashelf.dev>
2024-02-15 09:16:22 +0300

    docs: add missing documentation entry; define renamed options

--- modules/utility/preview/glow/glow.nix
+++ modules/utility/preview/glow/glow.nix
@@ -1,12 +1,14 @@
 {lib, ...}: let
-  inherit (lib) mkEnableOption mkMappingOption;
+  inherit (lib) mkEnableOption mkMappingOption mkRenamedOptionModule;
 in {
+  imports = [
+    (mkRenamedOptionModule ["vim" "languages" "markdown" "glow" "enable"] ["vim" "utility" "preview" "glow" "enable"])
+  ];
+
   options.vim.utility.preview = {
     glow = {
       enable = mkEnableOption "markdown preview in neovim with glow";
-      mappings = {
-        openPreview = mkMappingOption "Open preview" "<leader>p";
-      };
+      mappings.openPreview = mkMappingOption "Open preview" "<leader>p";
     };
   };
 }
