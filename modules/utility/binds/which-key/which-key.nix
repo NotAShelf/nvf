29b9bd4db0c67a3b9d2bd4aa5ec6763d96997788
Frothy <76622149+FrothyMarrow@users.noreply.github.com>
2024-02-20 22:15:18 -0500

    utility/binds/which-key: allow configurable category labels

--- modules/utility/binds/which-key/which-key.nix
+++ modules/utility/binds/which-key/which-key.nix
@@ -1,7 +1,18 @@
-{lib, ...}: let
-  inherit (lib) mkEnableOption;
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.options) mkEnableOption mkOption;
+  inherit (lib.types) attrsOf str;
 in {
   options.vim.binds.whichKey = {
     enable = mkEnableOption "which-key keybind helper menu";
+
+    register = mkOption {
+      description = "Register label for which-key keybind helper menu";
+      type = attrsOf str;
+      default = {};
+    };
   };
 }
