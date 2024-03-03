59d5ac0d63c74f1420fa7cb2c0ca17b0fc66dfc1
Ching Pei Yang <badnam3o.0@gmail.com>
2024-03-03 16:48:12 +0000

    feat(session-manager): custom setup opts

--- modules/session/nvim-session-manager/nvim-session-manager.nix
+++ modules/session/nvim-session-manager/nvim-session-manager.nix
@@ -1,7 +1,23 @@
 {lib, ...}: let
-  inherit (lib.options) mkOption mkEnableOption;
-  inherit (lib.types) nullOr str bool int listOf enum;
+  inherit (lib.types) nullOr str bool;
+  inherit (lib) mkEnableOption mkOption types mkRenamedOptionModule;
 in {
+  imports = let
+    renameSetupOpt = oldPath: newPath:
+      mkRenamedOptionModule (["vim" "session" "nvim-session-manager"] ++ oldPath) (["vim" "session" "nvim-session-manager" "setupOpts"] ++ newPath);
+  in [
+    (renameSetupOpt ["pathReplacer"] ["path_replacer"])
+    (renameSetupOpt ["colonReplacer"] ["colon_replacer"])
+    (renameSetupOpt ["autoloadMode"] ["autoload_mode"])
+    (renameSetupOpt ["maxPathLength"] ["max_path_length"])
+    (renameSetupOpt ["autoSave" "lastSession"] ["autosave_last_session"])
+    (renameSetupOpt ["autoSave" "ignoreNotNormal"] ["autosave_ignore_not_normal"])
+    (renameSetupOpt ["autoSave" "ignoreDirs"] ["autosave_ignore_dirs"])
+    (renameSetupOpt ["autoSave" "ignoreFiletypes"] ["autosave_ignore_filetypes"])
+    (renameSetupOpt ["autoSave" "ignoreBufTypes"] ["autosave_ignore_buftypes"])
+    (renameSetupOpt ["autoSave" "onlyInSession"] ["autosave_only_in_session"])
+  ];
+
   options.vim.session.nvim-session-manager = {
     enable = mkEnableOption "nvim-session-manager: manage sessions like folders in VSCode";
 
@@ -37,63 +53,63 @@ in {
       description = "Whether or not we should use dressing.nvim to build a session picker UI";
     };
 
-    pathReplacer = mkOption {
-      type = str;
-      default = "__";
-      description = "The character to which the path separator will be replaced for session files";
-    };
+    setupOpts = {
+      path_replacer = mkOption {
+        type = types.str;
+        default = "__";
+        description = "The character to which the path separator will be replaced for session files";
+      };
 
-    colonReplacer = mkOption {
-      type = str;
-      default = "++";
-      description = "The character to which the colon symbol will be replaced for session files";
-    };
+      colon_replacer = mkOption {
+        type = types.str;
+        default = "++";
+        description = "The character to which the colon symbol will be replaced for session files";
+      };
 
-    autoloadMode = mkOption {
-      type = enum ["Disabled" "CurrentDir" "LastSession"];
-      default = "LastSession";
-      description = "Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession";
-    };
+      autoload_mode = mkOption {
+        type = types.enum ["Disabled" "CurrentDir" "LastSession"];
+        default = "LastSession";
+        description = "Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession";
+      };
 
-    maxPathLength = mkOption {
-      type = nullOr int;
-      default = 80;
-      description = "Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all";
-    };
+      max_path_length = mkOption {
+        type = types.nullOr types.int;
+        default = 80;
+        description = "Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all";
+      };
 
-    autoSave = {
-      lastSession = mkOption {
-        type = bool;
+      autosave_last_session = mkOption {
+        type = types.bool;
         default = true;
         description = "Automatically save last session on exit and on session switch";
       };
 
-      ignoreNotNormal = mkOption {
-        type = bool;
+      autosave_ignore_not_normal = mkOption {
+        type = types.bool;
         default = true;
         description = "Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed";
       };
 
-      ignoreDirs = mkOption {
-        type = listOf str;
+      autosave_ignore_dirs = mkOption {
+        type = types.listOf types.str;
         default = [];
         description = "A list of directories where the session will not be autosaved";
       };
 
-      ignoreFiletypes = mkOption {
-        type = listOf str;
+      autosave_ignore_filetypes = mkOption {
+        type = types.listOf types.str;
         default = ["gitcommit"];
         description = "All buffers of these file types will be closed before the session is saved";
       };
 
-      ignoreBufTypes = mkOption {
-        type = listOf str;
+      autosave_ignore_buftypes = mkOption {
+        type = types.listOf types.str;
         default = [];
         description = "All buffers of these bufer types will be closed before the session is saved";
       };
 
-      onlyInSession = mkOption {
-        type = bool;
+      autosave_only_in_session = mkOption {
+        type = types.bool;
         default = false;
         description = "Always autosaves session. If true, only autosaves after a session is active";
       };
