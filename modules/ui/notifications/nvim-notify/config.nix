80fee9dae724cc695d00c8937996eeb7dae893e9
Ching Pei Yang <badnam3o.0@gmail.com>
2024-03-10 20:41:33 +0000

    feat(nvim-notify): custom setup opts

--- modules/ui/notifications/nvim-notify/config.nix
+++ modules/ui/notifications/nvim-notify/config.nix
@@ -5,26 +5,16 @@
 }: let
   inherit (lib.modules) mkIf;
   inherit (lib.nvim.dag) entryAnywhere;
+  inherit (lib.nvim.lua) toLuaObject;
 
   cfg = config.vim.notify.nvim-notify;
 in {
   config = mkIf cfg.enable {
     vim = {
       startPlugins = ["nvim-notify"];
+
       luaConfigRC.nvim-notify = entryAnywhere ''
-        require('notify').setup {
-          stages = "${cfg.stages}",
-          timeout = ${toString cfg.timeout},
-          background_colour = "${cfg.background_colour}",
-          position = "${cfg.position}",
-          icons = {
-              ERROR = "${cfg.icons.ERROR}",
-              WARN = "${cfg.icons.WARN}",
-              INFO = "${cfg.icons.INFO}",
-              DEBUG = "${cfg.icons.DEBUG}",
-              TRACE = "${cfg.icons.TRACE}",
-          },
-        }
+        require('notify').setup(${toLuaObject cfg.setupOpts})
 
         -- required to fix offset_encoding errors
         local notify = vim.notify
