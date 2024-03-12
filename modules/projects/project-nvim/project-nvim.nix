b54032f3f3268594094da79f28d688a3a2fe74a3
NotAShelf <raf@notashelf.dev>
2024-03-12 03:48:38 +0300

    modules/projects: switch to explicit lib calls

--- modules/projects/project-nvim/project-nvim.nix
+++ modules/projects/project-nvim/project-nvim.nix
@@ -1,60 +1,57 @@
-{
-  config,
-  lib,
-  ...
-}: let
-  inherit (lib) mkEnableOption mkOption types;
+{lib, ...}: let
+  inherit (lib.options) mkEnableOption mkOption;
+  inherit (lib.types) enum bool listOf str;
 in {
   options.vim.projects.project-nvim = {
     enable = mkEnableOption "project-nvim for project management";
 
     manualMode = mkOption {
-      type = types.bool;
+      type = bool;
       default = true;
       description = "don't automatically change the root directory so the user has the option to manually do so using `:ProjectRoot` command";
     };
 
     # detection methods should accept one or more strings from a list
     detectionMethods = mkOption {
-      type = types.listOf types.str;
+      type = listOf str;
       default = ["lsp" "pattern"];
       description = "Detection methods to use";
     };
 
     # patterns
     patterns = mkOption {
-      type = types.listOf types.str;
+      type = listOf str;
       default = [".git" "_darcs" ".hg" ".bzr" ".svn" "Makefile" "package.json" "flake.nix" "cargo.toml"];
       description = "Patterns to use for pattern detection method";
     };
 
     # table of lsp servers to ignore by name
     lspIgnored = mkOption {
-      type = types.listOf types.str;
+      type = listOf str;
       default = [];
       description = "LSP servers no ignore by name";
     };
 
     excludeDirs = mkOption {
-      type = types.listOf types.str;
+      type = listOf str;
       default = [];
       description = "Directories to exclude from project root search";
     };
 
     showHidden = mkOption {
-      type = types.bool;
+      type = bool;
       default = false;
       description = "Show hidden files in telescope picker";
     };
 
     silentChdir = mkOption {
-      type = types.bool;
+      type = bool;
       default = true;
       description = "Silently change directory when changing project";
     };
 
     scopeChdir = mkOption {
-      type = types.enum ["global" "tab" "win"];
+      type = enum ["global" "tab" "win"];
       default = "global";
       description = "What scope to change the directory";
     };
