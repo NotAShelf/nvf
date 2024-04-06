893742f6e9df239379df9fdfdd7785df689b8dc3
Pei Yang Ching <pei.ching@check24.de>
2024-04-06 19:12:58 +0200

    cleanup: remove more nvim-compe

--- modules/autopairs/nvim-autopairs/nvim-autopairs.nix
+++ modules/autopairs/nvim-autopairs/nvim-autopairs.nix
@@ -1,10 +1,10 @@
 {lib, ...}: let
-  inherit (lib) mkRenamedOptionModule;
+  inherit (lib) mkRemovedOptionModule;
   inherit (lib.options) mkEnableOption mkOption;
-  inherit (lib.types) enum bool;
+  inherit (lib.types) enum;
 in {
   imports = [
-    # (mkRenamedOptionModule ["vim" "autopairs" "nvim-compe"] ["vim" "autopairs" "nvim-compe" "setupOpts"])
+    (mkRemovedOptionModule ["vim" "autopairs" "nvim-compe"] "nvim-compe is deprecated and no longer suported.")
   ];
 
   options.vim = {
@@ -15,26 +15,6 @@ in {
         type = enum ["nvim-autopairs"];
         default = "nvim-autopairs";
         description = "Set the autopairs type. Options: nvim-autopairs [nvim-autopairs]";
-      };
-
-      nvim-compe.setupOpts = lib.nvim.types.mkPluginSetupOption "nvim-compe" {
-        map_cr = mkOption {
-          type = bool;
-          default = true;
-          description = ''map <CR> on insert mode'';
-        };
-
-        map_complete = mkOption {
-          type = bool;
-          default = true;
-          description = "auto insert `(` after select function or method item";
-        };
-
-        auto_select = mkOption {
-          type = bool;
-          default = false;
-          description = "auto select first item";
-        };
       };
     };
   };
