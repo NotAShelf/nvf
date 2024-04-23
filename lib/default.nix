b614dc6b4139035a9c33554831aec4b439955f39
NotAShelf <raf@notashelf.dev>
2024-04-23 16:17:09 +0300

    lib/lists: init; add `listContainsValue`
    Helps us validate lists that contain a bunch of values and see if it contains a desired list of values

--- lib/default.nix
+++ lib/default.nix
@@ -9,6 +9,7 @@
   binds = import ./binds.nix {inherit lib;};
   dag = import ./dag.nix {inherit lib;};
   languages = import ./languages.nix {inherit lib;};
+  lists = import ./lists.nix {inherit lib;};
   lua = import ./lua.nix {inherit lib;};
   vim = import ./vim.nix;
 }
