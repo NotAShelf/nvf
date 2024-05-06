6eba2a5585e3cb479a85cac0cd0c125d19fe28a9
NotAShelf <raf@notashelf.dev>
2024-05-06 22:57:27 +0300

    languages: move bash & dart configurations to standalone files

--- modules/plugins/languages/bash/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  imports = [
-    ./bash.nix
-    ./config.nix
-  ];
-}
--- modules/plugins/languages/dart.nix
+++ modules/plugins/languages/dart.nix
@@ -1,18 +1,23 @@
 {
   config,
-  lib,
   pkgs,
+  lib,
   ...
 }: let
   inherit (builtins) attrNames;
+  inherit (lib.modules) mkIf mkMerge;
+  inherit (lib.trivial) boolToString;
   inherit (lib.lists) isList;
   inherit (lib.options) mkEnableOption mkOption;
   inherit (lib.types) enum either listOf package nullOr str bool;
   inherit (lib.strings) optionalString;
   inherit (lib.nvim.lua) expToLua;
   inherit (lib.nvim.types) mkGrammarOption;
+  inherit (lib.nvim.dag) entryAnywhere;
 
   cfg = config.vim.languages.dart;
+  ftcfg = cfg.flutter-tools;
+
   defaultServer = "dart";
   servers = {
     dart = {
@@ -48,15 +53,16 @@ in {
         default = defaultServer;
       };
       package = mkOption {
-        description = "Dart LSP server package, or the command to run as a list of strings";
-        example = ''[lib.getExe pkgs.jdt-language-server "-data" "~/.cache/jdtls/workspace"]'';
         type = either package (listOf str);
         default = servers.${cfg.lsp.server}.package;
+        example = ''[lib.getExe pkgs.jdt-language-server "-data" "~/.cache/jdtls/workspace"]'';
+        description = "Dart LSP server package, or the command to run as a list of strings";
       };
+
       opts = mkOption {
-        description = "Options to pass to Dart LSP server";
         type = nullOr str;
         default = null;
+        description = "Options to pass to Dart LSP server";
       };
     };
 
@@ -70,23 +76,26 @@ in {
 
     flutter-tools = {
       enable = mkOption {
-        description = "Enable flutter-tools for flutter support";
         type = bool;
         default = config.vim.languages.enableLSP;
+        description = "Enable flutter-tools for flutter support";
       };
 
       enableNoResolvePatch = mkOption {
-        description = ''
-          Patch flutter-tools so that it doesn't resolve symlinks when detecting flutter path.
-          This is required if you want to use a flutter package built with nix.
-          If you are using a flutter SDK installed from a different source and encounter the error "`dart` missing from PATH", disable this option.
-        '';
         type = bool;
         default = true;
+        description = ''
+          Whether to patch flutter-tools so that it doesn't resolve
+          symlinks when detecting flutter path.
+
+          This is required if you want to use a flutter package built with nix.
+          If you are using a flutter SDK installed from a different source
+          and encounter the error "`dart` missing from PATH", disable this option.
+        '';
       };
 
       color = {
-        enable = mkEnableOption "Whether or mot to highlight color variables at all";
+        enable = mkEnableOption "highlighting color variables";
 
         highlightBackground = mkOption {
           type = bool;
@@ -112,4 +121,47 @@ in {
       };
     };
   };
+
+  config = mkIf cfg.enable (mkMerge [
+    (mkIf cfg.treesitter.enable {
+      vim.treesitter.enable = true;
+      vim.treesitter.grammars = [cfg.treesitter.package];
+    })
+
+    (mkIf cfg.lsp.enable {
+      vim.lsp.lspconfig.enable = true;
+
+      vim.lsp.lspconfig.sources.dart-lsp = servers.${cfg.lsp.server}.lspConfig;
+    })
+
+    (mkIf ftcfg.enable {
+      vim.startPlugins =
+        if ftcfg.enableNoResolvePatch
+        then ["flutter-tools-patched"]
+        else ["flutter-tools"];
+
+      vim.luaConfigRC.flutter-tools = entryAnywhere ''
+        require('flutter-tools').setup {
+          lsp = {
+            color = { -- show the derived colours for dart variables
+              enabled = ${boolToString ftcfg.color.enable}, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+              background = ${boolToString ftcfg.color.highlightBackground}, -- highlight the background
+              foreground = ${boolToString ftcfg.color.highlightForeground}, -- highlight the foreground
+              virtual_text = ${boolToString ftcfg.color.virtualText.enable}, -- show the highlight using virtual text
+              virtual_text_str = ${ftcfg.color.virtualText.character} -- the virtual text character to highlight
+            },
+
+            capabilities = capabilities,
+            on_attach = default_on_attach;
+            flags = lsp_flags,
+          },
+          ${optionalString cfg.dap.enable ''
+          debugger = {
+            enabled = true,
+          },
+        ''}
+        }
+      '';
+    })
+  ]);
 }
