b7af4beb1b0f3c5c782784f6879849d4f0a0b3a6
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2024-07-10 00:28:18 +0200

    lz.n: load lz.n

--- modules/wrapper/lazy/config.nix
+++ modules/wrapper/lazy/config.nix
@@ -3,12 +3,26 @@
   config,
   ...
 }: let
+  inherit (builtins) toJSON;
   inherit (lib.modules) mkIf;
+  inherit (lib.attrsets) mapAttrsToList;
+  inherit (lib.generators) mkLuaInline;
+  inherit (lib.nvim.lua) toLuaObject;
+  inherit (lib.nvim.dag) entryAnywhere;
   cfg = config.vim.lazy;
+
+  toLznSpec = name: plugin:
+    (removeAttrs plugin ["package"])
+    // {__HACK = mkLuaInline "nil, [1] = ${toJSON name}";};
+  lznSpecs = mapAttrsToList toLznSpec cfg.plugins;
 in {
   config.vim = mkIf cfg.enable {
     startPlugins = ["lz-n"];
 
-    # optPlugins =
+    optPlugins = mapAttrsToList (_: plugin: plugin.package) cfg.plugins;
+
+    luaConfigRC.lzn-load = entryAnywhere ''
+      require('lz.n').load(${toLuaObject lznSpecs})
+    '';
   };
 }
