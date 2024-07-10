33808b02291c7ce1dbe6c8cb15083f61592f2120
Pei Yang Ching <59727193+horriblename@users.noreply.github.com>
2024-07-10 13:00:41 +0200

    switch to other hacky array-table syntax

--- modules/wrapper/lazy/config.nix
+++ modules/wrapper/lazy/config.nix
@@ -3,18 +3,16 @@
   config,
   ...
 }: let
-  inherit (builtins) toJSON;
   inherit (lib.modules) mkIf;
   inherit (lib.attrsets) mapAttrsToList;
-  inherit (lib.generators) mkLuaInline;
   inherit (lib.nvim.lua) toLuaObject;
   inherit (lib.nvim.dag) entryAnywhere;
   cfg = config.vim.lazy;
 
-  toLznSpec = name: plugin:
+  toLuaLznSpec = name: plugin:
     (removeAttrs plugin ["package"])
-    // {__HACK = mkLuaInline "nil, [1] = ${toJSON name}";};
-  lznSpecs = mapAttrsToList toLznSpec cfg.plugins;
+    // {"@1" = name;};
+  lznSpecs = mapAttrsToList toLuaLznSpec cfg.plugins;
 in {
   config.vim = mkIf cfg.enable {
     startPlugins = ["lz-n"];
