a4d35b441986f4747abc47e618810325de38d6ce
raf <raf@notashelf.dev>
2024-07-14 18:30:12 +0000

    plugins/neo-tree: init module (#248)
    * plugins/neo-tree: init module
    
    * filetree/neo-tree: add setup options until border styles
    
    * plugins/filetree: rename module name
    
    * docs: update changelogs
    
    Mention the addition of neo-tree.

---
+++ modules/plugins/filetree/neo-tree/config.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  inherit (lib.nvim.dag) entryAnywhere;
+  inherit (lib.nvim.lua) toLuaObject;
+
+  cfg = config.vim.filetree.neo-tree;
+in {
+  config = mkIf cfg.enable {
+    vim = {
+      startPlugins = [
+        # dependencies
+        "plenary-nvim" # commons library
+        "image-nvim" # optional for image previews
+        "nui-nvim" # ui library
+        # neotree
+        "neo-tree-nvim"
+      ];
+
+      luaConfigRC.neo-tree = entryAnywhere ''
+        require("neo-tree").setup(${toLuaObject cfg.setupOpts})
+      '';
+    };
+  };
+}
