d73d07787bb871bdd2dd90a055e016028620cae2
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2024-08-03 14:45:29 +0200

    remove unused

--- lib/types/plugins.nix
+++ lib/types/plugins.nix
@@ -58,13 +58,6 @@
   };
 
   lznKeysSpec = submodule {
-    apply = x:
-      x
-      // {
-        "@1" = x.lhs;
-        "@2" = x.rhs;
-      };
-
     options = {
       desc = mkOption {
         description = "Description of the key map";
