a3f4b7b147198d94dac576b591c450a33d2e3fe0
NotAShelf <raf@notashelf.dev>
2024-09-07 02:57:57 +0300

    binds/which-key: start migrating to setupOpts

--- modules/plugins/utility/binds/which-key/config.nix
+++ modules/plugins/utility/binds/which-key/config.nix
@@ -4,33 +4,21 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
-  inherit (lib.strings) optionalString;
   inherit (lib.nvim.lua) toLuaObject;
   inherit (lib.nvim.dag) entryAnywhere;
 
   cfg = config.vim.binds.whichKey;
 in {
   config = mkIf cfg.enable {
-    vim.startPlugins = ["which-key"];
+    vim = {
+      startPlugins = ["which-key"];
 
-    vim.pluginRC.whichkey = entryAnywhere ''
-      local wk = require("which-key")
-      wk.setup ({
-        key_labels = {
-          ["<space>"] = "SPACE",
-          ["<leader>"] = "SPACE",
-          ["<cr>"] = "RETURN",
-          ["<tab>"] = "TAB",
-        },
+      pluginRC.whichkey = entryAnywhere ''
+        local wk = require("which-key")
 
-        ${optionalString config.vim.ui.borders.plugins.which-key.enable ''
-        window = {
-          border = ${toLuaObject config.vim.ui.borders.plugins.which-key.style},
-        },
-      ''}
-      })
-
-      wk.register(${toLuaObject cfg.register})
-    '';
+        wk.setup (${toLuaObject cfg.setupOpts})
+        wk.register(${toLuaObject cfg.register})
+      '';
+    };
   };
 }
