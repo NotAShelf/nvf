5db7e2aff92af2402749b8fc1d5379b5957798ff
Charlie Root <charlie@charlieroot.dev>
2024-10-05 13:01:39 +0200

    lib/types: rename custom.nix to types.nix, mov theme.nix into types.nix

--- lib/types/default.nix
+++ lib/types/default.nix
@@ -6,12 +6,10 @@
   typesDag = import ./dag.nix {inherit lib;};
   typesPlugin = import ./plugins.nix {inherit inputs lib;};
   typesLanguage = import ./languages.nix {inherit lib;};
-  typesCustom = import ./custom.nix {inherit lib;};
-  typesTheme = import ./theme.nix {inherit lib;};
+  typesTypes = import ./types.nix {inherit lib;};
 in {
   inherit (typesDag) dagOf;
   inherit (typesPlugin) pluginsOpt extraPluginType mkPluginSetupOption luaInline pluginType borderType;
   inherit (typesLanguage) diagnostics mkGrammarOption;
-  inherit (typesCustom) anythingConcatLists char;
-  inherit (typesTheme) hexColorType;
+  inherit (typesTypes) anythingConcatLists char hexColor;
 }
