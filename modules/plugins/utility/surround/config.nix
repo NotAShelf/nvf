4701bb15c5be93a80af92c03380297c69c8dbef1
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-06 20:13:39 +0200

    surround-nvim: lazy load

--- modules/plugins/utility/surround/config.nix
+++ modules/plugins/utility/surround/config.nix
@@ -8,25 +8,50 @@
   inherit (lib.nvim.lua) toLuaObject;
 
   cfg = config.vim.utility.surround;
+  vendoredKeybinds = {
+    insert = "<C-g>z";
+    insert_line = "<C-g>Z";
+    normal = "gz";
+    normal_cur = "gZ";
+    normal_line = "gzz";
+    normal_cur_line = "gZZ";
+    visual = "gz";
+    visual_line = "gZ";
+    delete = "gzd";
+    change = "gzr";
+    change_line = "gZR";
+  };
+  mkLznKey = mode: key: {
+    inherit key mode;
+  };
 in {
   config = mkIf cfg.enable {
     vim = {
       startPlugins = ["nvim-surround"];
       pluginRC.surround = entryAnywhere "require('nvim-surround').setup(${toLuaObject cfg.setupOpts})";
 
-      utility.surround.setupOpts.keymaps = mkIf cfg.useVendoredKeybindings {
-        insert = "<C-g>z";
-        insert_line = "<C-g>Z";
-        normal = "gz";
-        normal_cur = "gZ";
-        normal_line = "gzz";
-        normal_cur_line = "gZZ";
-        visual = "gz";
-        visual_line = "gZ";
-        delete = "gzd";
-        change = "gzr";
-        change_line = "gZR";
-      };
+      lazy.plugins = [
+        {
+          package = "nvim-surround";
+          setupModule = "nvim-surround";
+          inherit (cfg) setupOpts;
+
+          keys =
+            map (mkLznKey ["i"]) (with vendoredKeybinds; [insert insert_line])
+            ++ map (mkLznKey ["x"]) (with vendoredKeybinds; [visual visual_line])
+            ++ map (mkLznKey ["n"]) (with vendoredKeybinds; [
+              normal
+              normal_cur
+              normal_line
+              normal_cur_line
+              delete
+              change
+              change_line
+            ]);
+        }
+      ];
+
+      utility.surround.setupOpts.keymaps = mkIf cfg.useVendoredKeybindings vendoredKeybinds;
     };
   };
 }
