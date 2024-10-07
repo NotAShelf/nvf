a8491b5aa1911b4abb92c6f9af065386554f7514
NotAShelf <raf@notashelf.dev>
2024-10-07 03:26:22 +0300

    nvim-cursorline: fix docs

--- modules/plugins/visuals/nvim-cursorline/nvim-cursorline.nix
+++ modules/plugins/visuals/nvim-cursorline/nvim-cursorline.nix
@@ -14,17 +14,25 @@ in {
 
   options.vim.visuals.nvim-cursorline = {
     enable = mkEnableOption "cursor word and line highlighting [nvim-cursorline]";
+
+    # Upstream has **zero** documentation whatsoever. I'm making wild assumptions
+    # on what goes into description based don the source code. I'm sorry. Not.
     setupOpts = mkPluginSetupOption "nvim-cursorline" {
       cursorline = {
         enable = mkEnableOption "cursor line highlighting";
         timeout = mkOption {
           type = int;
           default = 1000;
+          description = "Cursorline timeout";
         };
 
         number = mkOption {
           type = bool;
           default = false;
+          description = ''
+            If true, `vim.wo.cursorlineopt` will be set to "number"
+            when the trigger conditions are met.
+          '';
         };
       };
 
@@ -33,16 +41,23 @@ in {
         timeout = mkOption {
           type = int;
           default = 1000;
+          description = "Cursorword timeout";
         };
 
         min_length = mkOption {
           type = int;
           default = 3;
+          description = ''
+            The min_length option defines the minimum number of characters
+            a word must have to be highlighted as a "cursor word." Any word
+            shorter than this value will be ignored and not highlighted.
+          '';
         };
 
         hl.underline = mkOption {
           type = bool;
           default = true;
+          description = "Whether to underline matching cursorword";
         };
       };
     };
