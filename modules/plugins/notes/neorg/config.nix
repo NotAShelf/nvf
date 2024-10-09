e1d292aac3158e0a69c8544b43b17795ebface57
Soliprem <franci.solidoro@gmail.com>
2024-10-09 10:18:12 +0200

    neorg: setupOpts setup correctly

--- modules/plugins/notes/neorg/config.nix
+++ modules/plugins/notes/neorg/config.nix
@@ -28,24 +28,7 @@ in {
         };
 
         pluginRC.neorg = entryAnywhere ''
-          -- Treesitter configuration
-          require('nvim-treesitter.configs').setup {
-
-            -- If TS highlights are not enabled at all, or disabled via `disable` prop,
-            -- highlighting will fallback to default Vim syntax highlighting
-            highlight = {
-              enable = true,
-              -- Required for spellcheck, some LaTex highlights and
-              -- code block highlights that do not have ts grammar
-              additional_vim_regex_highlighting = {'neorg'},
-            },
-          }
-
-          require('neorg').setup{
-          ${cfg.setupOpts}
-          }
-
-          vim.wo.conceallevel = 2
+          require('neorg').setup(${toLuaObject cfg.setupOpts})
         '';
       };
     }
--- modules/plugins/notes/neorg/neorg.nix
+++ modules/plugins/notes/neorg/neorg.nix
@@ -4,33 +4,13 @@
   pkgs,
   ...
 }: let
-  inherit (lib.options) mkEnableOption mkOption;
-  inherit (lib.types) lines;
+  inherit (lib.options) mkEnableOption;
   inherit (lib.nvim.types) mkGrammarOption mkPluginSetupOption;
 in {
   options.vim.notes.neorg = {
     enable = mkEnableOption "neorg: Neovim plugin for Neorg";
 
-    setupOpts = mkOption {
-      type = lines;
-      default = ''
-
-        load = {
-          ['core.defaults'] = {}, -- Loads default behaviour
-          ['core.concealer'] = {}, -- Adds pretty icons to your documents
-          ['core.export'] = {}, -- Adds export options
-          ['core.integrations.telescope'] = {}, -- Telescope integration
-          ['core.dirman'] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = '~/Documents/neorg',
-              },
-            },
-          },
-        },
-      '';
-      description = "Neorg configuration";
-    };
+    setupOpts = mkPluginSetupOption "Neorg" {};
 
     treesitter = {
       enable = mkEnableOption "Neorg treesitter" // {default = config.vim.languages.enableTreesitter;};
