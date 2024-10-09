90795a506a307cf82fc98c6e003bb76072301e6d
Soliprem <franci.solidoro@gmail.com>
2024-10-09 10:40:11 +0200

    neorg: sane default for setupOpts

--- modules/plugins/notes/neorg/neorg.nix
+++ modules/plugins/notes/neorg/neorg.nix
@@ -4,13 +4,32 @@
   pkgs,
   ...
 }: let
-  inherit (lib.options) mkEnableOption;
+  inherit (lib.options) mkEnableOption mkOption;
+  inherit (lib.types) submodule listOf str;
   inherit (lib.nvim.types) mkGrammarOption mkPluginSetupOption;
 in {
   options.vim.notes.neorg = {
     enable = mkEnableOption "neorg: Neovim plugin for Neorg";
 
-    setupOpts = mkPluginSetupOption "Neorg" {};
+    setupOpts = mkPluginSetupOption "Neorg" {
+      load = {
+        "core.defaults" = mkOption {
+          default = {};
+
+          type = submodule {
+            options = {
+              enable = mkEnableOption "A wrapper to interface with several different completion engines.";
+              config = {
+                disable = mkOption {
+                  type = listOf str;
+                  default = [];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
 
     treesitter = {
       enable = mkEnableOption "Neorg treesitter" // {default = config.vim.languages.enableTreesitter;};
