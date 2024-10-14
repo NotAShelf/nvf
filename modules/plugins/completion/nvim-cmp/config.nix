faba694a477f3097d226f6fb5db5c8850f3c1aa9
Pei Yang Ching <59727193+horriblename@users.noreply.github.com>
2024-10-14 22:24:22 +0200

    fix: bad cmp confirm

--- modules/plugins/completion/nvim-cmp/config.nix
+++ modules/plugins/completion/nvim-cmp/config.nix
@@ -60,19 +60,9 @@ in {
         ${mappings.confirm.value} = mkLuaInline ''
           cmp.mapping(function(fallback)
             if cmp.visible() then
-              ${
-            if luasnipEnable
-            then ''
-              if luasnip.expandable() then
-                luasnip.expand()
-              else
-                cmp.confirm({ select = true })
-              end
-            ''
-            else "cmp.confirm({ select = true })"
-          }
+              cmp.confirm({ select = true })
             else
-                fallback()
+              fallback()
             end
           end)
         '';
