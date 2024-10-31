7543cc272512b687a9dae45c3c24169bcf7f8f36
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-31 13:57:13 +0100

    toggleterm: make lazygit keybind optional

--- modules/plugins/terminal/toggleterm/config.nix
+++ modules/plugins/terminal/toggleterm/config.nix
@@ -5,6 +5,7 @@
 }: let
   inherit (builtins) toJSON;
   inherit (lib.strings) optionalString;
+  inherit (lib.lists) optional;
   inherit (lib.modules) mkIf;
   inherit (lib.meta) getExe;
   inherit (lib.nvim.binds) mkLznBinding;
@@ -17,13 +18,12 @@ in {
       lazy.plugins.toggleterm-nvim = {
         package = "toggleterm-nvim";
         cmd = ["ToggleTerm" "ToggleTermSendCurrentLine" "ToggleTermSendVisualLines" "ToggleTermSendVisualSelection" "ToggleTermSetName" "ToggleTermToggleAll"];
-        keys = [
-          (mkLznBinding ["n"] cfg.mappings.open "<Cmd>execute v:count . \"ToggleTerm\"<CR>" "Toggle terminal")
-          {
+        keys =
+          [(mkLznBinding ["n"] cfg.mappings.open "<Cmd>execute v:count . \"ToggleTerm\"<CR>" "Toggle terminal")]
+          ++ optional cfg.lazygit.enable {
             key = cfg.lazygit.mappings.open;
             desc = lazygitMapDesc;
-          }
-        ];
+          };
 
         setupModule = "toggleterm";
         inherit (cfg) setupOpts;
