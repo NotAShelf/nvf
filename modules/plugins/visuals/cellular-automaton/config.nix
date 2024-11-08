a6bb6e1b3e360a1222bdc877633ac37440f1d4d2
NotAShelf <raf@notashelf.dev>
2024-11-08 12:56:21 +0300

    modules/visuals: migrate plugins to setupOpts

---
+++ modules/plugins/visuals/cellular-automaton/config.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  inherit (lib.strings) optionalString;
+  inherit (lib.nvim.lua) toLuaObject;
+  inherit (lib.nvim.dag) entryAnywhere entryAfter;
+  inherit (lib.nvim.binds) mkBinding;
+
+  cfg = config.vim.visuals.cellular-automaton;
+in {
+  config = mkIf cfg.enable {
+    vim = {
+      startPlugins = ["cellular-automaton"];
+
+      maps.normal = mkBinding cfg.mappings.makeItRain "<cmd>CellularAutomaton make_it_rain<CR>" "Make it rain";
+
+      pluginRC = {
+        # XXX: This has no error handling. User can set
+        # `animation.setup` to a bogus value, and we would
+        # have an error in our hands. I don't think there
+        # is a good way to check for errors, so I'm leaving
+        # it like this under the assumption that the user
+        # will not mess it up for no reason.
+        cellular-automaton-anim = entryAnywhere (optionalString cfg.animation.register ''
+          -- Coerce user animation config into pluginRC
+          ${toLuaObject cfg.animation.setup}
+        '');
+
+        cellular-automaton = entryAfter ["cellular-automaton-anim"] ''
+          -- Register the animation
+          require("cellular-automaton").register_animation(ca_config)
+        '';
+      };
+    };
+  };
+}
---
+++ modules/plugins/visuals/indent-blankline/config.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  inherit (lib.nvim.lua) toLuaObject;
+  inherit (lib.nvim.dag) entryAnywhere;
+
+  cfg = config.vim.visuals.indent-blankline;
+in {
+  config = mkIf cfg.enable {
+    vim = {
+      startPlugins = ["indent-blankline"];
+
+      pluginRC.indent-blankline = entryAnywhere ''
+        require("ibl").setup(${toLuaObject cfg.setupOpts})
+      '';
+    };
+  };
+}
