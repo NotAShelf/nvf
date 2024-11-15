66f5e1edf4f468a093d3b82ae3873bd62e77f5f5
diniamo <diniamo53@gmail.com>
2024-11-15 08:06:00 +0100

    telescope: add missing mappings, cleanup

--- modules/plugins/utility/telescope/config.nix
+++ modules/plugins/utility/telescope/config.nix
@@ -34,36 +34,37 @@ in {
 
         keys =
           [
-            (mkSetLznBinding "n" mappings.findFiles "<cmd> Telescope find_files<CR>")
-            (mkSetLznBinding "n" mappings.liveGrep "<cmd> Telescope live_grep<CR>")
-            (mkSetLznBinding "n" mappings.buffers "<cmd> Telescope buffers<CR>")
-            (mkSetLznBinding "n" mappings.helpTags "<cmd> Telescope help_tags<CR>")
-            (mkSetLznBinding "n" mappings.open "<cmd> Telescope<CR>")
+            (mkSetLznBinding "n" mappings.findFiles "<cmd>Telescope find_files<CR>")
+            (mkSetLznBinding "n" mappings.liveGrep "<cmd>Telescope live_grep<CR>")
+            (mkSetLznBinding "n" mappings.buffers "<cmd>Telescope buffers<CR>")
+            (mkSetLznBinding "n" mappings.helpTags "<cmd>Telescope help_tags<CR>")
+            (mkSetLznBinding "n" mappings.open "<cmd>Telescope<CR>")
+            (mkSetLznBinding "n" mappings.resume "<cmd>Telescope resume<CR>")
 
-            (mkSetLznBinding "n" mappings.gitCommits "<cmd> Telescope git_commits<CR>")
-            (mkSetLznBinding "n" mappings.gitBufferCommits "<cmd> Telescope git_bcommits<CR>")
-            (mkSetLznBinding "n" mappings.gitBranches "<cmd> Telescope git_branches<CR>")
-            (mkSetLznBinding "n" mappings.gitStatus "<cmd> Telescope git_status<CR>")
-            (mkSetLznBinding "n" mappings.gitStash "<cmd> Telescope git_stash<CR>")
+            (mkSetLznBinding "n" mappings.gitCommits "<cmd>Telescope git_commits<CR>")
+            (mkSetLznBinding "n" mappings.gitBufferCommits "<cmd>Telescope git_bcommits<CR>")
+            (mkSetLznBinding "n" mappings.gitBranches "<cmd>Telescope git_branches<CR>")
+            (mkSetLznBinding "n" mappings.gitStatus "<cmd>Telescope git_status<CR>")
+            (mkSetLznBinding "n" mappings.gitStash "<cmd>Telescope git_stash<CR>")
           ]
           ++ (optionals config.vim.lsp.enable [
-            (mkSetLznBinding "n" mappings.lspDocumentSymbols "<cmd> Telescope lsp_document_symbols<CR>")
-            (mkSetLznBinding "n" mappings.lspWorkspaceSymbols "<cmd> Telescope lsp_workspace_symbols<CR>")
+            (mkSetLznBinding "n" mappings.lspDocumentSymbols "<cmd>Telescope lsp_document_symbols<CR>")
+            (mkSetLznBinding "n" mappings.lspWorkspaceSymbols "<cmd>Telescope lsp_workspace_symbols<CR>")
 
-            (mkSetLznBinding "n" mappings.lspReferences "<cmd> Telescope lsp_references<CR>")
-            (mkSetLznBinding "n" mappings.lspImplementations "<cmd> Telescope lsp_implementations<CR>")
-            (mkSetLznBinding "n" mappings.lspDefinitions "<cmd> Telescope lsp_definitions<CR>")
-            (mkSetLznBinding "n" mappings.lspTypeDefinitions "<cmd> Telescope lsp_type_definitions<CR>")
-            (mkSetLznBinding "n" mappings.diagnostics "<cmd> Telescope diagnostics<CR>")
+            (mkSetLznBinding "n" mappings.lspReferences "<cmd>Telescope lsp_references<CR>")
+            (mkSetLznBinding "n" mappings.lspImplementations "<cmd>Telescope lsp_implementations<CR>")
+            (mkSetLznBinding "n" mappings.lspDefinitions "<cmd>Telescope lsp_definitions<CR>")
+            (mkSetLznBinding "n" mappings.lspTypeDefinitions "<cmd>Telescope lsp_type_definitions<CR>")
+            (mkSetLznBinding "n" mappings.diagnostics "<cmd>Telescope diagnostics<CR>")
           ])
           ++ (
             optionals config.vim.treesitter.enable [
-              (mkSetLznBinding "n" mappings.treesitter "<cmd> Telescope treesitter<CR>")
+              (mkSetLznBinding "n" mappings.treesitter "<cmd>Telescope treesitter<CR>")
             ]
           )
           ++ (
             optionals config.vim.projects.project-nvim.enable [
-              (mkSetLznBinding "n" mappings.findProjects "<cmd Telescope projects<CR>")
+              (mkSetLznBinding "n" mappings.findProjects "<cmd>Telescope projects<CR>")
             ]
           );
       };
