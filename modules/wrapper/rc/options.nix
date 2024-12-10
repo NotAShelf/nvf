66005a51c380cc8ca3597b1e092cb343d900ad93
NotAShelf <raf@notashelf.dev>
2024-12-11 00:08:48 +0300

    wrappr/rc: allow strings in `vim.options.signcolumn`

--- modules/wrapper/rc/options.nix
+++ modules/wrapper/rc/options.nix
@@ -6,6 +6,7 @@
   inherit (lib.options) mkOption mkEnableOption literalMD literalExpression;
   inherit (lib.strings) optionalString;
   inherit (lib.types) str bool int enum attrsOf lines listOf either path submodule anything;
+  inherit (lib.trivial) isBool;
   inherit (lib.nvim.languages) toVimBool;
   inherit (lib.nvim.types) dagOf;
   inherit (lib.nvim.lua) listToLuaTable;
@@ -230,9 +231,12 @@ in {
           };
 
           signcolumn = mkOption {
-            type = bool;
+            type = either str bool;
             default = true;
-            apply = x: toVimBool x; # convert to a yes/no str
+            apply = x:
+              if isBool x
+              then toVimBool x # convert to a yes/no str
+              else x;
             description = "Show the sign column";
           };
         };
