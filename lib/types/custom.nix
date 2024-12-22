795240fac9743426c43c6962b53af444b50b23bd
Ching Pei Yang <badnam3o.0@gmail.com>
2024-12-22 23:00:26 +0100

    lib: remove anythingConcatLists

--- lib/types/custom.nix
+++ lib/types/custom.nix
@@ -1,57 +1,8 @@
 {lib}: let
-  inherit (lib.options) showOption showFiles getFiles mergeOneOption mergeEqualOption;
-  inherit (lib.strings) isString isStringLike;
-  inherit (lib.types) anything attrsOf listOf mkOptionType;
-  inherit (lib.nvim.types) anythingConcatLists;
-  inherit (builtins) typeOf isAttrs any head concatLists stringLength match;
+  inherit (lib.options) mergeEqualOption;
+  inherit (lib.strings) isString stringLength match;
+  inherit (lib.types) listOf mkOptionType;
 in {
-  # HACK: Does this break anything in our case?
-  # A modified version of the nixpkgs anything type that concatenates lists
-  # This isn't the default because the order in which the lists are concatenated depends on the order in which the modules are imported,
-  # which makes it non-deterministic
-  anythingConcatLists =
-    anything
-    // {
-      merge = loc: defs: let
-        getType = value:
-          if isAttrs value && isStringLike value
-          then "stringCoercibleSet"
-          else typeOf value;
-
-        # Throw an error if not all defs have the same type
-        checkType = getType (head defs).value;
-        commonType =
-          if any (def: getType def.value != checkType) defs
-          then throw "The option `${showOption loc}' has conflicting option types in ${showFiles (getFiles defs)}"
-          else checkType;
-
-        mergeFunctions = {
-          # Recursively merge attribute sets
-          set = (attrsOf anythingConcatLists).merge;
-
-          # Overridden behavior for lists, that concatenates lists
-          list = _: defs: concatLists (map (e: e.value) defs);
-
-          # This means it's a package, only accept a single definition
-          stringCoercibleSet = mergeOneOption;
-
-          # This works by passing the argument to the functions,
-          # and merging their returns values instead
-          lambda = loc: defs: arg:
-            anythingConcatLists.merge
-            (loc ++ ["<function body>"])
-            (map (def: {
-                inherit (def) file;
-                value = def.value arg;
-              })
-              defs);
-        };
-      in
-        # Merge the defs with the correct function from above, if available
-        # otherwise only allow equal values
-        (mergeFunctions.${commonType} or mergeEqualOption) loc defs;
-    };
-
   mergelessListOf = elemType: let
     super = listOf elemType;
   in
