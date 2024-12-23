c94cf9a47c135fd88132e7399d99fc5829656241
Ching Pei Yang <badnam3o.0@gmail.com>
2024-12-23 02:51:53 +0100

    blink: add luasnip integration

--- modules/plugins/completion/blink-cmp/config.nix
+++ modules/plugins/completion/blink-cmp/config.nix
@@ -4,16 +4,38 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
-  cfg = config.vim.autocomplete.blink-nvim;
+  inherit (lib.generators) mkLuaInline;
+  cfg = config.vim.autocomplete.blink-cmp;
 in {
   vim = mkIf cfg.enable {
-    lazy.plugins = [
-      {
-        package = "blink-cmp";
-        setupModule = "blink";
-        inherit (cfg) setupOpts;
-        event = ["InsertEnter" "CmdlineEnter"];
-      }
-    ];
+    lazy.plugins.blink-cmp = {
+      package = "blink-cmp";
+      setupModule = "blink.cmp";
+      inherit (cfg) setupOpts;
+      event = ["InsertEnter" "CmdlineEnter"];
+    };
+
+    autocomplete = {
+      enableSharedCmpSources = true;
+
+      blink-cmp.setupOpts = {
+        snippets = mkIf config.vim.snippets.luasnip.enable {
+          expand = mkLuaInline ''
+            function(snippet)
+              return require("luasnip").lsp_expand(snippet)
+            end
+          '';
+          active = mkLuaInline ''
+            function(filter)
+              if filter and filter.direction then
+                return require('luasnip').jumpable(filter.direction)
+              end
+              return require('luasnip').in_snippet()
+            end
+          '';
+          jump = mkLuaInline "function(direction) require('luasnip').jump(direction) end";
+        };
+      };
+    };
   };
 }
