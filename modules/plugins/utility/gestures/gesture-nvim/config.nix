85347de09d6eaa03989a98885781f9c8fd31830b
NotAShelf <raf@notashelf.dev>
2025-01-04 16:08:11 +0300

    utility/gesture-nvim: convert to `vim.options`

--- modules/plugins/utility/gestures/gesture-nvim/config.nix
+++ modules/plugins/utility/gestures/gesture-nvim/config.nix
@@ -15,43 +15,47 @@
   mappings = addDescriptionsToMappings cfg.mappings mappingDefinitions;
 in {
   config = mkIf cfg.enable {
-    vim.startPlugins = ["gesture-nvim"];
+    vim = {
+      startPlugins = ["gesture-nvim"];
 
-    vim.maps.normal = mkMerge [
-      (mkSetLuaBinding mappings.draw "require('gesture').draw")
-      (mkSetLuaBinding mappings.finish "require('gesture').finish")
-      (mkIf (mappings.draw.value == "<RightDrag>") {
-        "<RightMouse>" = {action = "<Nop>";};
-      })
-    ];
+      maps.normal = mkMerge [
+        (mkSetLuaBinding mappings.draw "require('gesture').draw")
+        (mkSetLuaBinding mappings.finish "require('gesture').finish")
+        (mkIf (mappings.draw.value == "<RightDrag>") {
+          "<RightMouse>" = {action = "<Nop>";};
+        })
+      ];
 
-    vim.pluginRC.gesture-nvim = entryAnywhere ''
-      vim.opt.mouse = "a"
+      options.mouse = "a";
+      pluginRC.gesture-nvim = entryAnywhere ''
+        local gesture = require("gesture")
+        gesture.register({
+          name = "scroll to bottom",
+          inputs = { gesture.up(), gesture.down() },
+          action = "normal! G",
+        })
 
-      local gesture = require("gesture")
-      gesture.register({
-        name = "scroll to bottom",
-        inputs = { gesture.up(), gesture.down() },
-        action = "normal! G",
-      })
-      gesture.register({
-        name = "next tab",
-        inputs = { gesture.right() },
-        action = "tabnext",
-      })
-      gesture.register({
-        name = "previous tab",
-        inputs = { gesture.left() },
-        action = function(ctx) -- also can use callable
-          vim.cmd.tabprevious()
-        end,
-      })
-      gesture.register({
-        name = "go back",
-        inputs = { gesture.right(), gesture.left() },
-        -- map to `<C-o>` keycode
-        action = [[lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-o>", true, false, true), "n", true)]],
-      })
-    '';
+        gesture.register({
+          name = "next tab",
+          inputs = { gesture.right() },
+          action = "tabnext",
+        })
+
+        gesture.register({
+          name = "previous tab",
+          inputs = { gesture.left() },
+          action = function(ctx) -- also can use callable
+            vim.cmd.tabprevious()
+          end,
+        })
+
+        gesture.register({
+          name = "go back",
+          inputs = { gesture.right(), gesture.left() },
+          -- map to `<C-o>` keycode
+          action = [[lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-o>", true, false, true), "n", true)]],
+        })
+      '';
+    };
   };
 }
