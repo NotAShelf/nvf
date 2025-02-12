6b0f11006b5515d2010f2751b9fbae4c0f9f74c8
Joe Hanson <joe@veri.dev>
2025-02-12 10:17:07 -0600

    Update modules/plugins/hydra/hydra.nix
    place hydra plugin in utility folder
    
    Co-authored-by: raf <raf@notashelf.dev>

--- modules/plugins/hydra/hydra.nix
+++ modules/plugins/hydra/hydra.nix
@@ -2,7 +2,7 @@
   #inherit (lib) types;
   inherit (lib.options) mkEnableOption;
 in {
-  options.vim.hydra = {
-    enable = mkEnableOption "Creating custom submodes and menus [nvimtools/hydra.nvim]";
+  options.vim.utility.hydra = {
+    enable = mkEnableOption "utility for creating custom submodes and menus [nvimtools/hydra.nvim]";
   };
 }
