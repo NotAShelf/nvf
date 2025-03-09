5e9255923b64a28976e0b19b64b266bbf05fd415
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2025-03-09 14:03:19 +0300

    dashboard/alpha: Initial nix config implementation

--- modules/plugins/dashboard/alpha/alpha.nix
+++ modules/plugins/dashboard/alpha/alpha.nix
@@ -1,7 +1,29 @@
 {lib, ...}: let
-  inherit (lib.options) mkEnableOption;
+  inherit (lib.options) mkEnableOption mkOption;
+  inherit (lib.types) listOf attrsOf anything nullOr enum;
 in {
   options.vim.dashboard.alpha = {
-    enable = mkEnableOption "fast and fully programmable greeter for neovim [alpha.mvim]";
+    enable = mkEnableOption "fast and fully programmable greeter for neovim [alpha.nvim]";
+    theme = mkOption {
+      default = null;
+      type = nullOr (enum ["dashboard" "startify" "theta"]);
+      description = ''
+        Alpha default theme to use.
+      '';
+    };
+    layout = mkOption {
+      default = [];
+      type = listOf (attrsOf anything);
+      description = ''
+        Alpha dashboard layout.
+      '';
+    };
+    opts = mkOption {
+      default = {};
+      type = attrsOf anything;
+      description = ''
+        Optional global options.
+      '';
+    };
   };
 }
