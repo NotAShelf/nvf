e473a4ddb15090e791b202531e165e969d600b93
raf <me@notashelf.dev>
2025-03-18 09:52:07 +0000

    lsp/lspsaga: update source; lazyload; remove keybinds (#724)
    * pins: point lspsaga to new source
    
    Stop using the fork, the author is back.
    
    * pins: point lspsaga to new source
    
    Stop using the fork, the author is back.

--- modules/plugins/lsp/lspsaga/config.nix
+++ modules/plugins/lsp/lspsaga/config.nix
@@ -3,47 +3,24 @@
   lib,
   ...
 }: let
-  inherit (lib.modules) mkIf mkMerge;
-  inherit (lib.nvim.dag) entryAnywhere;
-  inherit (lib.nvim.binds) addDescriptionsToMappings mkSetLuaBinding;
-  inherit (lib.nvim.lua) toLuaObject;
+  inherit (lib.modules) mkIf mkDefault;
 
   cfg = config.vim.lsp;
-  self = import ./lspsaga.nix {inherit config lib;};
-
-  mappingDefinitions = self.options.vim.lsp.lspsaga.mappings;
-  mappings = addDescriptionsToMappings cfg.lspsaga.mappings mappingDefinitions;
 in {
   config = mkIf (cfg.enable && cfg.lspsaga.enable) {
     vim = {
-      startPlugins = ["lspsaga-nvim"];
+      lazy.plugins.lspsaga-nvim = {
+        package = "lspsaga-nvim";
+        setupModule = "lspsaga";
+        inherit (cfg.lspsaga) setupOpts;
 
-      pluginRC.lspsaga = entryAnywhere ''
-        require('lspsaga').init_lsp_saga(${toLuaObject cfg.lspsaga.setupOpts})
-      '';
-
-      maps = {
-        visual = mkSetLuaBinding mappings.codeAction "require('lspsaga.codeaction').range_code_action";
-        normal = mkMerge [
-          (mkSetLuaBinding mappings.lspFinder "require('lspsaga.provider').lsp_finder")
-          (mkSetLuaBinding mappings.renderHoveredDoc "require('lspsaga.hover').render_hover_doc")
-
-          (mkSetLuaBinding mappings.smartScrollUp "function() require('lspsaga.action').smart_scroll_with_saga(-1) end")
-          (mkSetLuaBinding mappings.smartScrollDown "function() require('lspsaga.action').smart_scroll_with_saga(1) end")
-
-          (mkSetLuaBinding mappings.rename "require('lspsaga.rename').rename")
-          (mkSetLuaBinding mappings.previewDefinition "require('lspsaga.provider').preview_definition")
-
-          (mkSetLuaBinding mappings.showLineDiagnostics "require('lspsaga.diagnostic').show_line_diagnostics")
-          (mkSetLuaBinding mappings.showCursorDiagnostics "require('lspsaga.diagnostic').show_cursor_diagnostics")
-
-          (mkSetLuaBinding mappings.nextDiagnostic "require('lspsaga.diagnostic').navigate('next')")
-          (mkSetLuaBinding mappings.previousDiagnostic "require('lspsaga.diagnostic').navigate('prev')")
-
-          (mkSetLuaBinding mappings.codeAction "require('lspsaga.codeaction').code_action")
-          (mkIf (!cfg.lspSignature.enable) (mkSetLuaBinding mappings.signatureHelp "require('lspsaga.signaturehelp').signature_help"))
-        ];
+        event = ["LspAttach"];
       };
+
+      # Optional dependencies, pretty useful to enhance default functionality of
+      # Lspsaga.
+      treesitter.enable = mkDefault true;
+      visuals.nvim-web-devicons.enable = mkDefault true;
     };
   };
 }
--- modules/plugins/lsp/lspsaga/lspsaga.nix
+++ modules/plugins/lsp/lspsaga/lspsaga.nix
@@ -3,10 +3,21 @@
   lib,
   ...
 }: let
+  inherit (lib.modules) mkRemovedOptionModule;
   inherit (lib.options) mkOption mkEnableOption;
-  inherit (lib.nvim.binds) mkMappingOption;
   inherit (lib.nvim.types) borderType mkPluginSetupOption;
 in {
+  imports = [
+    (mkRemovedOptionModule ["vim" "lsp" "lspsaga" "mappings"] ''
+      Lspsaga mappings have been removed from nvf, as the original author has made
+      very drastic changes to the API after taking back ownership, and the fork we
+      used is now archived. Please refer to Lspsaga documentation to add keybinds
+      for functionality you have used.
+
+      <https://nvimdev.github.io/lspsaga>
+    '')
+  ];
+
   options.vim.lsp.lspsaga = {
     enable = mkEnableOption "LSP Saga";
 
@@ -16,27 +27,6 @@ in {
         default = config.vim.ui.borders.globalStyle;
         description = "Border type, see {command}`:help nvim_open_win`";
       };
-    };
-
-    mappings = {
-      lspFinder = mkMappingOption "LSP Finder [LSPSaga]" "<leader>lf";
-      renderHoveredDoc = mkMappingOption "Rendered hovered docs [LSPSaga]" "<leader>lh";
-
-      smartScrollUp = mkMappingOption "Smart scroll up [LSPSaga]" "<C-f>";
-      smartScrollDown = mkMappingOption "Smart scroll up [LSPSaga]" "<C-b>";
-
-      rename = mkMappingOption "Rename [LSPSaga]" "<leader>lr";
-      previewDefinition = mkMappingOption "Preview definition [LSPSaga]" "<leader>ld";
-
-      showLineDiagnostics = mkMappingOption "Show line diagnostics [LSPSaga]" "<leader>ll";
-      showCursorDiagnostics = mkMappingOption "Show cursor diagnostics [LSPSaga]" "<leader>lc";
-
-      nextDiagnostic = mkMappingOption "Next diagnostic [LSPSaga]" "<leader>ln";
-      previousDiagnostic = mkMappingOption "Previous diagnostic [LSPSaga]" "<leader>lp";
-
-      codeAction = mkMappingOption "Code action [LSPSaga]" "<leader>ca";
-
-      signatureHelp = mkMappingOption "Signature help [LSPSaga]" "<leader>ls";
     };
   };
 }
