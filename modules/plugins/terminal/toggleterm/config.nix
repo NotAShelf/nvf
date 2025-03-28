696fda23fe0115b5d706b02a65348f7c4c2592d0
Al Duncanson <alDuncanson@proton.me>
2025-03-28 01:31:41 -0400

    terminal/slides: add slides plugin
    Add the terminal slides plugin to open markdown presentations in a
    floating terminal.

--- modules/plugins/terminal/toggleterm/config.nix
+++ modules/plugins/terminal/toggleterm/config.nix
@@ -12,39 +12,83 @@
 
   cfg = config.vim.terminal.toggleterm;
   lazygitMapDesc = "Open lazygit [toggleterm]";
+  slidesMapDesc = "Open slides [toggleterm]";
 in {
   config = mkIf cfg.enable {
     vim = {
       lazy.plugins.toggleterm-nvim = {
         package = "toggleterm-nvim";
-        cmd = ["ToggleTerm" "ToggleTermSendCurrentLine" "ToggleTermSendVisualLines" "ToggleTermSendVisualSelection" "ToggleTermSetName" "ToggleTermToggleAll"];
+        cmd = [
+          "ToggleTerm"
+          "ToggleTermSendCurrentLine"
+          "ToggleTermSendVisualLines"
+          "ToggleTermSendVisualSelection"
+          "ToggleTermSetName"
+          "ToggleTermToggleAll"
+        ];
         keys =
-          [(mkKeymap "n" cfg.mappings.open "<Cmd>execute v:count . \"ToggleTerm\"<CR>" {desc = "Toggle terminal";})]
+          [
+            (mkKeymap "n" cfg.mappings.open "<Cmd>execute v:count . \"ToggleTerm\"<CR>" {
+              desc = "Toggle terminal";
+            })
+          ]
           ++ optional cfg.lazygit.enable {
             key = cfg.lazygit.mappings.open;
             mode = "n";
             desc = lazygitMapDesc;
+          }
+          ++ optional cfg.slides.enable {
+            key = cfg.slides.mappings.open;
+            mode = "n";
+            desc = slidesMapDesc;
           };
 
         setupModule = "toggleterm";
         inherit (cfg) setupOpts;
-        after = optionalString cfg.lazygit.enable ''
-          local terminal = require 'toggleterm.terminal'
-          local lazygit = terminal.Terminal:new({
-            cmd = '${
-            if (cfg.lazygit.package != null)
-            then getExe cfg.lazygit.package
-            else "lazygit"
-          }',
-            direction = '${cfg.lazygit.direction}',
-            hidden = true,
-            on_open = function(term)
-              vim.cmd("startinsert!")
-            end
-          })
+        after =
+          optionalString cfg.lazygit.enable ''
+            local terminal = require 'toggleterm.terminal'
+            local lazygit = terminal.Terminal:new({
+              cmd = '${
+              if (cfg.lazygit.package != null)
+              then getExe cfg.lazygit.package
+              else "lazygit"
+            }',
+              direction = '${cfg.lazygit.direction}',
+              hidden = true,
+              on_open = function(term)
+                vim.cmd("startinsert!")
+              end
+            })
 
-          vim.keymap.set('n', ${toLuaObject cfg.lazygit.mappings.open}, function() lazygit:toggle() end, {silent = true, noremap = true, desc = '${lazygitMapDesc}'})
-        '';
+            vim.keymap.set('n', ${toLuaObject cfg.lazygit.mappings.open}, function() lazygit:toggle() end, {silent = true, noremap = true, desc = '${lazygitMapDesc}'})
+          ''
+          + optionalString cfg.slides.enable ''
+            local terminal = require 'toggleterm.terminal'
+
+            local file_path = vim.fn.expand("%:p")
+
+            if file_path == "" then
+              print("No file open")
+              return
+            end
+
+            local slides = terminal.Terminal:new({
+              cmd = '${
+              if (cfg.slides.package != null)
+              then getExe cfg.slides.package
+              else "slides"
+            } ' .. file_path,
+              direction = 'float',
+              hidden = true,
+              close_on_exit = true,
+              on_open = function(term)
+                vim.cmd("startinsert!")
+              end
+            })
+
+            vim.keymap.set('n', ${toLuaObject cfg.slides.mappings.open}, function() slides:toggle() end, {silent = true, noremap = true, desc = '${slidesMapDesc}'})
+          '';
       };
     };
   };
