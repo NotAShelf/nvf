fa7fb83315e379206ec62bd0bdd4891784cc48cf
tebuevd <3241533+tebuevd@users.noreply.github.com>
2025-03-30 20:04:48 -0700

    Merge pull request #760 from tebuevd/fix-telescope-pickers-config
    telescope: correctly nest pickers under setupOpts

--- modules/plugins/utility/telescope/telescope.nix
+++ modules/plugins/utility/telescope/telescope.nix
@@ -8,6 +8,12 @@
   inherit (lib.nvim.binds) mkMappingOption;
   inherit (lib.nvim.types) mkPluginSetupOption luaInline;
   setupOptions = {
+    pickers.find_files.find_command = mkOption {
+      description = "cmd to use for finding files";
+      type = either (listOf str) luaInline;
+      default = ["${pkgs.fd}/bin/fd" "--type=file"];
+    };
+
     defaults = {
       vimgrep_arguments = mkOption {
         description = ''
@@ -26,11 +32,6 @@
           "--hidden"
           "--no-ignore"
         ];
-      };
-      pickers.find_command = mkOption {
-        description = "cmd to use for finding files";
-        type = either (listOf str) luaInline;
-        default = ["${pkgs.fd}/bin/fd"];
       };
       prompt_prefix = mkOption {
         description = "Shown in front of Telescope's prompt";
