b9badafebe2e492a958a3dd0ae3e03db3c127cd9
Ching Pei Yang <badnam3o.0@gmail.com>
2025-04-19 22:24:04 +0200

    fixup! package: add nvf-queries package

--- flake/legacyPackages/nvf-queries/queries/nix/injections.scm
+++ flake/legacyPackages/nvf-queries/queries/nix/injections.scm
@@ -33,3 +33,21 @@
       (string_fragment) @injection.content)
   ]
   (#set! injection.language "lua"))
+
+(apply_expression
+  function: (apply_expression
+    function: [
+      (variable_expression
+        name: (identifier) @_func)
+      ;; matches lib.nvim.bla.bla.entryBefore
+      (select_expression
+        expression: (variable_expression)
+        attrpath: (attrpath
+          attr: (identifier) @_func .))
+    ]
+    argument: _)
+  argument: (indented_string_expression
+    (string_fragment) @injection.content)
+  (#match? @_func "^entry(Anywhere|After|Before)$")
+  (#set! injection.language "lua")
+  (#set! injection.combined))
