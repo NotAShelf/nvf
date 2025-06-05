aa35ef0319e63f12d9a90458fea807b5e77224be
sjcobb <sjc@incyan.com>
2025-06-05 09:42:27 +0100

    fix clojure imports

--- modules/plugins/languages/clojure.nix
+++ modules/plugins/languages/clojure.nix
@@ -4,13 +4,15 @@
   lib,
   ...
 }: let
+  inherit (builtins) attrNames;
   inherit (lib.options) mkEnableOption mkOption;
   inherit (lib.modules) mkIf mkMerge;
   inherit (lib.meta) getExe;
   inherit (lib.lists) isList;
-  inherit (lib.types) either listOf package str;
+  inherit (lib.types) enum either listOf package str;
   inherit (lib.nvim.types) mkGrammarOption;
   inherit (lib.nvim.lua) expToLua;
+  inherit (lib.nvim.attrsets) mapListToAttrs;
 
   cfg = config.vim.languages.clojure;
 
