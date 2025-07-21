25f4d62f6a98b5b8baf1230cc8991a1884a3f209
poz <poz@poz.pet>
2025-07-21 12:26:15 +0200

    languages/cue: convert module

--- modules/plugins/languages/cue.nix
+++ modules/plugins/languages/cue.nix
@@ -4,30 +4,35 @@
   lib,
   ...
 }: let
-  inherit (lib.options) mkEnableOption mkOption;
+  inherit (lib.options) mkEnableOption;
+  inherit (lib.meta) getExe;
   inherit (lib.modules) mkIf mkMerge;
-  inherit (lib.types) package;
-  inherit (lib.nvim.types) mkGrammarOption;
+  inherit (lib.nvim.attrsets) mapListToAttrs;
+  inherit (lib.nvim.types) mkGrammarOption mkServersOption;
 
   cfg = config.vim.languages.cue;
+
+  defaultServers = ["cue"];
+  servers = {
+    cue = {
+      enable = true;
+      cmd = [(getExe pkgs.cue) "lsp"];
+      filetypes = ["cue"];
+      root_markers = ["cue.mod" ".git"];
+    };
+  };
 in {
   options.vim.languages.cue = {
     enable = mkEnableOption "CUE language support";
 
     treesitter = {
       enable = mkEnableOption "CUE treesitter" // {default = config.vim.languages.enableTreesitter;};
-
       package = mkGrammarOption pkgs "cue";
     };
 
     lsp = {
       enable = mkEnableOption "CUE LSP support" // {default = config.vim.lsp.enable;};
-
-      package = mkOption {
-        type = package;
-        default = pkgs.cue;
-        description = "cue lsp implementation";
-      };
+      servers = mkServersOption "CUE" servers defaultServers;
     };
   };
 
@@ -38,14 +43,12 @@ in {
     })
 
     (mkIf cfg.lsp.enable {
-      vim.lsp.lspconfig.enable = true;
-      vim.lsp.lspconfig.sources.cue-lsp = ''
-        lspconfig.cue.setup {
-          capabilities = capabilities,
-          on_attach = default_on_attach,
-          cmd = {"${cfg.lsp.package}/bin/cue", "lsp"},
-        }
-      '';
+      vim.lsp.servers =
+        mapListToAttrs (n: {
+          name = n;
+          value = servers.${n};
+        })
+        cfg.lsp.servers;
     })
   ]);
 }
