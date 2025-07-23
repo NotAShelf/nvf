66e32eeafa5a1aee60349aa30f3f9d2d91c0a4ef
Cool-Game-Dev <softwaredevelopment.stingray177@passinbox.com>
2025-07-23 12:20:14 -0500

    utility/nvim-biscuits: Add pluginRC
    Add the setup function for the nvim-biscuits with setupOps to pluginRC.

--- modules/plugins/utility/nvim-biscuits/config.nix
+++ modules/plugins/utility/nvim-biscuits/config.nix
@@ -4,11 +4,17 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
+  inherit (lib.nvim.dag) entryAnywhere;
+  inherit (lib.nvim.lua) toLuaObject;
   cfg = config.vim.utility.nvim-biscuits;
 in {
   config = mkIf cfg.enable {
     vim = {
       startPlugins = ["nvim-biscuits"];
+
+      pluginRC.nvim-biscuits = entryAnywhere ''
+        require('nvim-biscuits').setup(${toLuaObject cfg.setupOpts})
+      '';
     };
   };
 }
