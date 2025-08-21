1f65e589bf2437b73a515235fc5f0122536aa3fd
rice-cracker-dev <ricecracker2234@proton.me>
2025-08-21 18:47:09 +0700

    languages/svelte: fix svelte-language-server not reloading .js/.ts files on change

--- modules/plugins/languages/svelte.nix
+++ modules/plugins/languages/svelte.nix
@@ -33,6 +33,15 @@
       '';
       on_attach = mkLuaInline ''
         function(client, bufnr)
+          vim.api.nvim_create_autocmd('BufWritePost', {
+            pattern = { '*.js', '*.ts' },
+            group = vim.api.nvim_create_augroup('svelte_js_ts_file_watch', {}),
+            callback = function(ctx)
+              -- internal API to sync changes that have not yet been saved to the file system
+              client:notify('$/onDidChangeTsOrJsFile', { uri = ctx.match })
+            end,
+          })
+
           vim.api.nvim_buf_create_user_command(bufnr, 'LspMigrateToSvelte5', function()
             client:exec_cmd({
               command = 'migrate_to_svelte_5',
