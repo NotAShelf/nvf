e814f8e8bef00bfab0a1af0260474a0f139e4688
siggsy <ziga.leskovec@placz.net>
2025-10-17 09:41:09 +0200

    languages/haskell: remove invalid vim.g options
    Haskell is not configured with lspconfig, but rather
    with haskell-tools.nvim. This commit fixes "conversion"
    made in 39efdc799.
    
    Without this, users are met with nvim errors about invalid
    vim.g options

--- modules/plugins/languages/haskell.nix
+++ modules/plugins/languages/haskell.nix
@@ -21,9 +21,7 @@
   defaultServers = ["hls"];
   servers = {
     hls = {
-      enable = false;
       cmd = [(getExe' pkgs.haskellPackages.haskell-language-server "haskell-language-server-wrapper") "--lsp"];
-      filetypes = ["haskell" "lhaskell"];
       on_attach =
         mkLuaInline
         /*
@@ -42,17 +40,6 @@
               end, opts)
               vim.keymap.set('n', '<localleader>rq', ht.repl.quit, opts)
             end
-        '';
-      root_dir =
-        mkLuaInline
-        /*
-        lua
-        */
-        ''
-          function(bufnr, on_dir)
-            local fname = vim.api.nvim_buf_get_name(bufnr)
-            on_dir(util.root_pattern('hie.yaml', 'stack.yaml', 'cabal.project', '*.cabal', 'package.yaml')(fname))
-          end
         '';
       settings = {
         haskell = {
