d55680f4d29bcc606b3d96b4e2a8d4487d44bfae
Yoni FIRROLONI <pro@yoni-firroloni.com>
2025-12-11 22:21:50 +1300

    buildfix: fix rafs wrong code suggestion

--- modules/plugins/minimap/codewindow/config.nix
+++ modules/plugins/minimap/codewindow/config.nix
@@ -5,6 +5,7 @@
 }: let
   inherit (lib.modules) mkIf mkMerge;
   inherit (lib.nvim.dag) entryAnywhere;
+  inherit (lib.nvim.lua) toLuaObject;
   inherit (lib.nvim.binds) addDescriptionsToMappings mkSetLuaBinding pushDownDefault;
 
   cfg = config.vim.minimap.codewindow;
@@ -33,7 +34,7 @@ in {
 
       pluginRC.codewindow = entryAnywhere ''
         local codewindow = require('codewindow')
-        codewindow.setup(${cfg.setupOpts})
+        codewindow.setup(${toLuaObject cfg.setupOpts})
       '';
     };
   };
