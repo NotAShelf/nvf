14f3029364a5537973fe00a19b11c72176e9d1c6
kazimazi <attila.koszo.official@gmail.com>
2026-02-08 10:48:19 +0100

    utility/grug-far-nvim: add new plugin

---
+++ modules/plugins/utility/grug-far-nvim/grug-far-nvim.nix
@@ -0,0 +1,12 @@
+{
+  lib,
+  ...
+}: let
+  inherit (lib.options) mkEnableOption;
+  inherit (lib.nvim.types) mkPluginSetupOption;
+in {
+  options.vim.utility.grug-far-nvim = {
+    enable = mkEnableOption "grug-far";
+    setupOpts = mkPluginSetupOption "grug-far" {};
+  };
+}
