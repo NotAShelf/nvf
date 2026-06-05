9852b32d4199d2f94f885265fabd7e388950afee
Samuel Cobb <sjcobb2003@gmail.com>
2026-06-06 00:55:31 +0100

    feat(rust): add mkLspPresetOptionWithDesc and be more explicit with punctuation

--- lib/types/lsp.nix
+++ lib/types/lsp.nix
@@ -1,12 +1,23 @@
 {lib}: let
-  inherit (lib.options) mkEnableOption;
+  inherit (lib.options) mkOption;
 
   mkLspPresetEnableOption = option: display: fileTypes:
-    mkEnableOption ''
-      the ${display} Language Server.
-      Default `filetypes = ${lib.generators.toPretty {} fileTypes}`.
-      Use {option}`vim.lsp.servers.${option}` for customization
-    '';
+    mkLspPresetEnableOptionWithDesc option display fileTypes "";
+
+  mkLspPresetEnableOptionWithDesc = option: display: fileTypes: description:
+    mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = lib.removeSuffix "\n" (''
+          the ${display} Language Server.
+          Default `filetypes = ${lib.generators.toPretty {} fileTypes}`.
+          Use {option}`vim.lsp.servers.${option}` for customization.
+        ''
+        + lib.optionalString (description != "") ''
+
+          ${description}
+        '');
+    };
 in {
-  inherit mkLspPresetEnableOption;
+  inherit mkLspPresetEnableOption mkLspPresetEnableOptionWithDesc;
 }
