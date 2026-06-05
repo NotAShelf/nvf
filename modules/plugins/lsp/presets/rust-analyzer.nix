7109ec7284424f5b7e1f416840c92238e0d447b7
Samuel Cobb <sjcobb2003@gmail.com>
2026-06-05 03:39:29 +0100

    feat(rust): remove now default settings

--- modules/plugins/lsp/presets/rust-analyzer.nix
+++ modules/plugins/lsp/presets/rust-analyzer.nix
@@ -153,39 +153,6 @@ in {
           };
         };
       };
-      settings = {
-        rust-analyzer = {
-          lens = {
-            debug = {
-              enable = true;
-            };
-            enable = true;
-            implementations = {
-              enable = true;
-            };
-            references = {
-              adt = {
-                enable = true;
-              };
-              enumVariant = {
-                enable = true;
-              };
-              method = {
-                enable = true;
-              };
-              trait = {
-                enable = true;
-              };
-            };
-            run = {
-              enable = true;
-            };
-            updateTest = {
-              enable = true;
-            };
-          };
-        };
-      };
     };
   };
 }
