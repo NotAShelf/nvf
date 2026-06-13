2fb70f907935529b493230de2866fd68ee2a9974
Snoweuph <snow@euph.email>
2026-06-13 18:04:53 +0200

    formatter/conform-nvim/presets: init

---
+++ modules/plugins/formatter/conform-nvim/presets/default.nix
@@ -0,0 +1,64 @@
+{
+  imports = [
+    # ./alejandra.nix
+    # ./asmfmt.nix
+    # ./astyle.nix
+    # ./bake.nix
+    # ./biome-check.nix
+    # ./biome-organize-imports.nix
+    # ./biome.nix
+    # ./black.nix
+    # ./cabal-fmt.nix
+    # ./clang-format.nix
+    # ./csharpier.nix
+    # ./deno.nix
+    # ./djlint.nix
+    # ./dockerfmt.nix
+    # ./fantomas.nix
+    # ./fish-indent.nix
+    # ./floskell.nix
+    # ./format-r.nix
+    # ./fourmolu.nix
+    # ./gersemi.nix
+    # ./gofmt.nix
+    # ./gofumpt.nix
+    # ./golines.nix
+    # ./hcl.nix
+    # ./indent.nix
+    # ./isort.nix
+    # ./jqfmt.nix
+    # ./jsonfmt.nix
+    # ./latexindent.nix
+    # ./mdformat.nix
+    # ./mix.nix
+    # ./nasmfmt.nix
+    # ./nimpretty.nix
+    # ./nixfmt.nix
+    # ./nomad.nix
+    # ./ocalformat.nix
+    # ./ormolu.nix
+    # ./php-cs-fixer.nix
+    # ./prettier.nix
+    # ./prettierd.nix
+    # ./qmlformat.nix
+    # ./rubocop.nix
+    # ./ruff.nix
+    # ./ruff-check.nix
+    # ./rumdl.nix
+    # ./rustfmt.nix
+    # ./shfmt.nix
+    # ./smlfmt.nix
+    # ./sqlfluff.nix
+    # ./sqruff.nix
+    # ./styler.nix
+    # ./stylish-haskell.nix
+    # ./stylua.nix
+    # ./superhtml.nix
+    ./taplo.nix
+    # ./terraform.nix
+    # ./tex-fmt.nix
+    # ./tofu.nix
+    ./tombi.nix
+    # ./typstyle.nix
+  ];
+}
