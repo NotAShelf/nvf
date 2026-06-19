dfa9b24743752e81ddb2cf116388c4decad72717
NotAShelf <raf@notashelf.dev>
2026-06-20 02:10:54 +0300

    plugins/mini: expose `setupOpts` for users
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I016430c8de4c819c785f92565701cbed6a6a6964

--- modules/plugins/mini/colors/colors.nix
+++ modules/plugins/mini/colors/colors.nix
@@ -1,7 +1,9 @@
 {lib, ...}: let
   inherit (lib.options) mkEnableOption;
+  inherit (lib.nvim.types) mkPluginSetupOption;
 in {
   options.vim.mini.colors = {
     enable = mkEnableOption "mini.colors";
+    setupOpts = mkPluginSetupOption "mini.colors" {};
   };
 }
