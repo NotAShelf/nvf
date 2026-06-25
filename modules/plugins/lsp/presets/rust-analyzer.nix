b7d3837a5554f9831841d304b718fe6279347057
Samuel Cobb <sjcobb2003@gmail.com>
2026-06-25 02:17:25 +0100

    rm filetypes line

--- modules/plugins/lsp/presets/rust-analyzer.nix
+++ modules/plugins/lsp/presets/rust-analyzer.nix
@@ -16,7 +16,6 @@ in {
     enable = mkLspPresetEnableOptionWith {
       option = "rust-analyzer";
       display = "Rust Analyzer";
-      fileTypes = [];
       description = ''Note: do not set `init_options` for this LS config, it will be automatically populated by the contents of settings["rust-analyzer"] per https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26'';
     };
   };
