7906bd04012ea568d13661406f65a619b77e37c1
Snoweuph <snow@euph.email>
2026-07-11 18:23:42 +0200

    languages/query: init

---
+++ modules/plugins/languages/query.nix
@@ -0,0 +1,63 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.options) literalExpression mkEnableOption mkOption;
+  inherit (lib.modules) mkIf mkMerge;
+  inherit (lib) genAttrs;
+  inherit (lib.types) enum listOf;
+  inherit (lib.nvim.types) mkGrammarOption;
+
+  cfg = config.vim.languages.query;
+
+  defaultServers = ["ts-query-ls"];
+  servers = ["ts-query-ls"];
+in {
+  options.vim.languages.query = {
+    enable = mkEnableOption "Treesitter Query support";
+
+    treesitter = {
+      enable =
+        mkEnableOption "Treesitter Query treesitter"
+        // {
+          default = config.vim.languages.enableTreesitter;
+          defaultText = literalExpression "config.vim.languages.enableTreesitter";
+        };
+      package = mkGrammarOption pkgs "query";
+    };
+
+    lsp = {
+      enable =
+        mkEnableOption "Treesitter Query LSP support"
+        // {
+          default = config.vim.lsp.enable;
+          defaultText = literalExpression "config.vim.lsp.enable";
+        };
+      servers = mkOption {
+        type = listOf (enum servers);
+        default = defaultServers;
+        description = "Treesitter Query LSP server to use";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    (mkIf cfg.treesitter.enable {
+      vim.treesitter = {
+        enable = true;
+        grammars = [cfg.treesitter.package];
+      };
+    })
+
+    (mkIf cfg.lsp.enable {
+      vim.lsp = {
+        presets = genAttrs cfg.lsp.servers (_: {enable = true;});
+        servers = genAttrs cfg.lsp.servers (_: {
+          filetypes = ["query"];
+        });
+      };
+    })
+  ]);
+}
