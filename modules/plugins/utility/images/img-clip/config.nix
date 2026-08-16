4e6775f1590305474e4d8289bd7a175235e9a0f2
Soliprem <franci.solidoro@gmail.com>
2026-08-16 20:53:41 +0200

    plugins/img-clip: lazy load on LazyFile

--- modules/plugins/utility/images/img-clip/config.nix
+++ modules/plugins/utility/images/img-clip/config.nix
@@ -4,20 +4,21 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
-  inherit (lib.nvim.dag) entryAnywhere;
-  inherit (lib.nvim.lua) toLuaObject;
 
   cfg = config.vim.utility.images.img-clip;
 in {
   config = mkIf cfg.enable {
-    vim = {
-      startPlugins = [
-        "img-clip"
+    vim.lazy.plugins.img-clip = {
+      package = "img-clip";
+      setupModule = "img-clip";
+      inherit (cfg) setupOpts;
+      event = [
+        {
+          event = "User";
+          pattern = "LazyFile";
+        }
       ];
-
-      pluginRC.img-clip = entryAnywhere ''
-        require("img-clip").setup(${toLuaObject cfg.setupOpts})
-      '';
+      cmd = ["PasteImage" "ImgClipDebug" "ImgClipConfig"];
     };
   };
 }
