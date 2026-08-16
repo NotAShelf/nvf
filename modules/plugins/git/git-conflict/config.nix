e97001e4c2e0351624ec6eb0eef6b493a605c94d
Soliprem <franci.solidoro@gmail.com>
2026-08-16 20:47:35 +0200

    plugins/git-conflict: lazy load on BufReadPre / BufNewFile

--- modules/plugins/git/git-conflict/config.nix
+++ modules/plugins/git/git-conflict/config.nix
@@ -4,33 +4,38 @@
   options,
   ...
 }: let
-  inherit (lib.modules) mkIf mkMerge;
+  inherit (lib.modules) mkIf;
   inherit (lib.nvim.binds) mkKeymap;
-  inherit (lib.nvim.dag) entryAnywhere;
-  inherit (lib.nvim.lua) toLuaObject;
 
   cfg = config.vim.git.git-conflict;
 
   inherit (options.vim.git.git-conflict) mappings;
 in {
-  config = mkIf cfg.enable (mkMerge [
-    {
-      vim = {
-        startPlugins = ["git-conflict-nvim"];
-
-        keymaps = [
-          (mkKeymap "n" cfg.mappings.ours "<Plug>(git-conflict-ours)" {desc = mappings.ours.description;})
-          (mkKeymap "n" cfg.mappings.theirs "<Plug>(git-conflict-theirs)" {desc = mappings.theirs.description;})
-          (mkKeymap "n" cfg.mappings.both "<Plug>(git-conflict-both)" {desc = mappings.both.description;})
-          (mkKeymap "n" cfg.mappings.none "<Plug>(git-conflict-none)" {desc = mappings.none.description;})
-          (mkKeymap "n" cfg.mappings.prevConflict "<Plug>(git-conflict-prev-conflict)" {desc = mappings.prevConflict.description;})
-          (mkKeymap "n" cfg.mappings.nextConflict "<Plug>(git-conflict-next-conflict)" {desc = mappings.nextConflict.description;})
-        ];
-
-        pluginRC.git-conflict = entryAnywhere ''
-          require('git-conflict').setup(${toLuaObject ({default_mappings = false;} // cfg.setupOpts)})
-        '';
-      };
-    }
-  ]);
+  config = mkIf cfg.enable {
+    vim.lazy.plugins.git-conflict-nvim = {
+      package = "git-conflict-nvim";
+      setupModule = "git-conflict";
+      setupOpts = {default_mappings = false;} // cfg.setupOpts;
+      event = ["BufReadPre" "BufNewFile"];
+      cmd = [
+        "GitConflictRefresh"
+        "GitConflictListQf"
+        "GitConflictChooseOurs"
+        "GitConflictChooseTheirs"
+        "GitConflictChooseBoth"
+        "GitConflictChooseBase"
+        "GitConflictChooseNone"
+        "GitConflictNextConflict"
+        "GitConflictPrevConflict"
+      ];
+      keys = [
+        (mkKeymap "n" cfg.mappings.ours "<Plug>(git-conflict-ours)" {desc = mappings.ours.description;})
+        (mkKeymap "n" cfg.mappings.theirs "<Plug>(git-conflict-theirs)" {desc = mappings.theirs.description;})
+        (mkKeymap "n" cfg.mappings.both "<Plug>(git-conflict-both)" {desc = mappings.both.description;})
+        (mkKeymap "n" cfg.mappings.none "<Plug>(git-conflict-none)" {desc = mappings.none.description;})
+        (mkKeymap "n" cfg.mappings.prevConflict "<Plug>(git-conflict-prev-conflict)" {desc = mappings.prevConflict.description;})
+        (mkKeymap "n" cfg.mappings.nextConflict "<Plug>(git-conflict-next-conflict)" {desc = mappings.nextConflict.description;})
+      ];
+    };
+  };
 }
