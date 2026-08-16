653f3abe89248b0cea9080180962dd7f727893f8
Soliprem <franci.solidoro@gmail.com>
2026-08-16 20:47:07 +0200

    plugins/conform-nvim: lazy load on LazyFile / cmd

--- modules/plugins/formatter/conform-nvim/config.nix
+++ modules/plugins/formatter/conform-nvim/config.nix
@@ -4,17 +4,21 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
-  inherit (lib.nvim.dag) entryAnywhere;
-  inherit (lib.nvim.lua) toLuaObject;
 
   cfg = config.vim.formatter.conform-nvim;
 in {
   config = mkIf cfg.enable {
-    vim = {
-      startPlugins = ["conform-nvim"];
-      pluginRC.conform-nvim = entryAnywhere ''
-        require("conform").setup(${toLuaObject cfg.setupOpts})
-      '';
+    vim.lazy.plugins.conform-nvim = {
+      package = "conform-nvim";
+      setupModule = "conform";
+      inherit (cfg) setupOpts;
+      event = [
+        {
+          event = "User";
+          pattern = "LazyFile";
+        }
+      ];
+      cmd = ["ConformInfo"];
     };
   };
 }
