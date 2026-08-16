9faba235823757dd3a1a1d098145e225ac1d9c9e
Soliprem <franci.solidoro@gmail.com>
2026-08-16 20:53:01 +0200

    plugins/smartcolumn: lazyload on BufReadPre / BufNewFile

--- modules/plugins/ui/smartcolumn/config.nix
+++ modules/plugins/ui/smartcolumn/config.nix
@@ -4,18 +4,15 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
-  inherit (lib.nvim.dag) entryAnywhere;
-  inherit (lib.nvim.lua) toLuaObject;
 
   cfg = config.vim.ui.smartcolumn;
 in {
   config = mkIf cfg.enable {
-    vim = {
-      startPlugins = ["smartcolumn-nvim"];
-
-      pluginRC.smartcolumn = entryAnywhere ''
-        require("smartcolumn").setup(${toLuaObject cfg.setupOpts})
-      '';
+    vim.lazy.plugins.smartcolumn-nvim = {
+      package = "smartcolumn-nvim";
+      setupModule = "smartcolumn";
+      inherit (cfg) setupOpts;
+      event = ["BufReadPre" "BufNewFile"];
     };
   };
 }
