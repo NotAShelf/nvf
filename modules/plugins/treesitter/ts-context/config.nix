22f245d30c73608cfd0a641a578077ff89a5a577
Soliprem <franci.solidoro@gmail.com>
2026-08-16 20:48:52 +0200

    plugins/ts-context: lazy load on LazyFile

--- modules/plugins/treesitter/ts-context/config.nix
+++ modules/plugins/treesitter/ts-context/config.nix
@@ -4,21 +4,22 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
-  inherit (lib.nvim.lua) toLuaObject;
-  inherit (lib.nvim.dag) entryAfter;
 
   inherit (config.vim) treesitter;
   cfg = treesitter.context;
 in {
   config = mkIf (treesitter.enable && cfg.enable) {
-    vim = {
-      startPlugins = ["nvim-treesitter-context"];
-
-      # set up treesitter-context after Treesitter. The ordering
-      # should not matter, but there is no harm in doing this
-      pluginRC.treesitter-context = entryAfter ["treesitter"] ''
-        require("treesitter-context").setup(${toLuaObject cfg.setupOpts})
-      '';
+    vim.lazy.plugins.nvim-treesitter-context = {
+      package = "nvim-treesitter-context";
+      setupModule = "treesitter-context";
+      inherit (cfg) setupOpts;
+      event = [
+        {
+          event = "User";
+          pattern = "LazyFile";
+        }
+      ];
+      cmd = ["TSContext"];
     };
   };
 }
