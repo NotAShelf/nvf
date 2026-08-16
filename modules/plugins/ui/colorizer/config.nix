052b25f6ed9a1f357c631cb86973ed57639de313
Soliprem <franci.solidoro@gmail.com>
2026-08-16 20:52:36 +0200

    plugins/colorizer: lazyload on BufReadPre / BufNewFile

--- modules/plugins/ui/colorizer/config.nix
+++ modules/plugins/ui/colorizer/config.nix
@@ -4,18 +4,15 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
-  inherit (lib.nvim.dag) entryAnywhere;
-  inherit (lib.nvim.lua) toLuaObject;
 
   cfg = config.vim.ui.colorizer;
 in {
   config = mkIf cfg.enable {
-    vim.startPlugins = [
-      "nvim-colorizer-lua"
-    ];
-
-    vim.pluginRC.colorizer = entryAnywhere ''
-      require('colorizer').setup(${toLuaObject cfg.setupOpts})
-    '';
+    vim.lazy.plugins.nvim-colorizer-lua = {
+      package = "nvim-colorizer-lua";
+      setupModule = "colorizer";
+      inherit (cfg) setupOpts;
+      event = ["BufReadPre" "BufNewFile"];
+    };
   };
 }
