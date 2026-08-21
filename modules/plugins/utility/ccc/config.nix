45ad22485d23bf131e3543eb7de16d0adf274e8b
Soliprem <franci.solidoro@gmail.com>
2026-08-21 16:37:34 +0200

    plugins/ccc: lazy load

--- modules/plugins/utility/ccc/config.nix
+++ modules/plugins/utility/ccc/config.nix
@@ -4,16 +4,27 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
-  inherit (lib.nvim.dag) entryAnywhere;
-  inherit (lib.nvim.lua) toLuaObject;
+  inherit (lib.attrsets) optionalAttrs;
   cfg = config.vim.utility.ccc;
 in {
   config = mkIf cfg.enable {
-    vim.startPlugins = ["ccc-nvim"];
-
-    vim.pluginRC.ccc = entryAnywhere ''
-      local ccc = require("ccc")
-      ccc.setup(${toLuaObject cfg.setupOpts})
-    '';
+    vim.lazy.plugins."ccc-nvim" =
+      {
+        package = "ccc-nvim";
+        setupModule = "ccc";
+        beforeSetup = ''local ccc = require("ccc")'';
+        # Omitted CccHighliterDisable since it would only ever be called as the first command
+        # if auto_enable was enabled, which would make the cmd option useless anyways
+        cmd = ["CccPick" "CccConvert" "CccHighlighterEnable" "CccHighlighterToggle"];
+        inherit (cfg) setupOpts;
+      }
+      // optionalAttrs (cfg.setupOpts.highlighter.auto_enable or false) {
+        event = [
+          {
+            event = "User";
+            pattern = "LazyFile";
+          }
+        ];
+      };
   };
 }
