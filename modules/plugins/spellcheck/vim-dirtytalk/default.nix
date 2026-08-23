460dad9e777df4008309f1d731a47b299dc7b41e
Snoweuph <snow@euph.email>
2026-08-23 21:46:16 +0200

    spellcheck: drop `vim-dirtytalk` (#1802)
    Closes: #1797

--- modules/plugins/spellcheck/vim-dirtytalk/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  imports = [
-    ./config.nix
-    ./vim-dirtytalk.nix
-  ];
-}
--- modules/plugins/spellcheck/vim-dirtytalk/vim-dirtytalk.nix
+++
@@ -1,7 +0,0 @@
-{lib, ...}: let
-  inherit (lib.modules) mkAliasOptionModule;
-in {
-  imports = [
-    (mkAliasOptionModule ["vim" "spellcheck" "vim-dirtytalk" "enable"] ["vim" "spellcheck" "programmingWordlist" "enable"])
-  ];
-}
