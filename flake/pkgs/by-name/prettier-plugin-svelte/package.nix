cd0ccf01bcb8b08c2af4bc417f166b24c8a2112b
NotAShelf <raf@notashelf.dev>
2026-09-26 21:43:37 +0300

    prettier-plugin-svelte: 3.5.2 -> 4.1.1; pin pnpm v10
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I40ad4e0796c34541b92f4af858c708666a6a6964

--- flake/pkgs/by-name/prettier-plugin-svelte/package.nix
+++ flake/pkgs/by-name/prettier-plugin-svelte/package.nix
@@ -1,28 +1,63 @@
 {
-  buildNpmPackage,
-  fetchFromGitHub,
+  lib,
   pins,
+  stdenv,
+  fetchFromGitHub,
+  nodejs,
+  pnpm_10,
+  pnpmConfigHook,
+  fetchPnpmDeps,
+  prettier,
 }: let
+  pnpm' = pnpm_10;
   pin = pins.prettier-plugin-svelte;
 in
-  buildNpmPackage (finalAttrs: {
+  stdenv.mkDerivation (finalAttrs: {
     pname = "prettier-plugin-svelte";
-    version = pin.version or pin.revision;
+    version = lib.removePrefix "prettier-plugin-svelte@" pin.version;
 
     src = fetchFromGitHub {
       inherit (pin.repository) owner repo;
-      rev = finalAttrs.version;
-      sha256 = pin.hash;
+      rev = pin.revision;
+      hash = pin.hash;
     };
 
-    npmDepsHash = "sha256-zejYnwkj6CBWOqA6LBYBEXMg0jT2vJqinBwzKdWIqpY=";
+    pnpmDeps = fetchPnpmDeps {
+      inherit (finalAttrs) pname version src;
+      pnpm = pnpm';
+      hash = "sha256-pPPATBjdVrkilllk9Xc6J2WWpERwmq6l0W0wUrWq7II=";
+      fetcherVersion = 4;
+    };
 
-    dontNpmPrune = true;
+    nativeBuildInputs = [
+      nodejs
+      pnpm'
+      (pnpmConfigHook.override {pnpm = pnpm';})
+    ];
 
-    # Fixes error: Cannot find module 'prettier'
-    postInstall = ''
-      pushd "$nodeModulesPath"
-      find -mindepth 1 -maxdepth 1 -type d -print0 | grep --null-data -Exv "\./(ulid|prettier)" | xargs -0 rm -rfv
-      popd
+    buildPhase = ''
+      runHook preBuild
+      pnpm run build
+      runHook postBuild
     '';
+
+    installPhase = ''
+      runHook preInstall
+
+      install -Dm644 -t $out plugin.js
+      # The plugin bundles everything except its peer dependencies, prettier
+      # and svelte. `svelte/compiler` resolves to a self-contained bundle, so
+      # the svelte package can be copied without its own dependencies.
+      mkdir $out/node_modules
+      cp -rL node_modules/svelte $out/node_modules/svelte
+      ln -s ${prettier}/lib/node_modules/prettier $out/node_modules/prettier
+
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Prettier plugin for Svelte";
+      homepage = "https://github.com/sveltejs/prettier-plugin-svelte";
+      license = lib.licenses.mit;
+    };
   })
